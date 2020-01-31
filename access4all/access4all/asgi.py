63ab220cc90047ec9c512e8969b5405849be400b
Nicolas Perriault <nicolas@perriault.net>
2020-01-31 12:22:01 +0100

    Adapt asgi env to Scalingo.

--- access4all/access4all/asgi.py
+++ access4all/access4all/asgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "access4all.settings")
 
-application = get_asgi_application()
+# Detect Scalingo environment
+if "SCALINGO_POSTGRESQL_URL" in os.environ:
+    from dj_static import Cling
+
+    application = Cling(get_wsgi_application())
+else:
+    application = get_asgi_application()
