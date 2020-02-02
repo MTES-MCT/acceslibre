c181dcf823f9aacb6aa006b753b06a9f865f7a80
Nicolas Perriault <nicolas@perriault.net>
2020-02-02 13:52:37 +0100

    WiP accessibility descriptors

--- erp/models.py
+++ erp/models.py
@@ -14,6 +14,108 @@ class Activite(models.Model):
         return self.nom
 
 
+class Circulation(models.Model):
+    RAMPE_FIXE = "fixe"
+    RAMPE_AMOVIBLE = "amovible"
+    RAMPE_CHOICES = [(RAMPE_FIXE, "Fixe"), (RAMPE_AMOVIBLE, "Amovible")]
+
+    DEVERS_LEGER = "léger"
+    DEVERS_IMPORTANT = "important"
+    DEVERS_CHOICES = [(DEVERS_LEGER, "Léger"), (DEVERS_IMPORTANT, "Important")]
+
+    PENTE_LEGERE = "légère"
+    PENTE_IMPORTANTE = "importante"
+    PENTE_CHOICES = [(PENTE_LEGERE, "Légère"), (PENTE_IMPORTANTE, "Importante")]
+
+    # équipements
+    reperage_vitres = models.BooleanField(null=True, blank=True, help_text="Présence d'un repérage sur les surfaces vitrées")
+    bande_guidage = models.BooleanField(null=True, blank=True, help_text="Présence d'une bande de guidage")
+    guidage_sonore = models.BooleanField(null=True, blank=True, help_text="Présence d'un dispositif de guidage sonore")
+    # largeur du passage
+    largeur_mini = models.IntegerField(null=True, blank=True, help_text="Largeur minimale de la circulation")
+    # déclivité
+    pente = models.CharField(max_length=15, null=True, blank=True, choices=PENTE_CHOICES, help_text="Présence et type de pente")
+    devers = models.CharField(
+        max_length=15, null=True, blank=True, choices=DEVERS_CHOICES, help_text="Présence et type de dévers"
+    )
+    rampe = models.CharField(null=True, blank=True, choices=RAMPE_CHOICES, help_text="Présence et type d'une rampe")
+    # escalier
+    escalier_marches = models.IntegerField(null=True, blank=True, help_text="Nombre de marches d'escalier, si applicable")
+    escalier_main_courante = models.BooleanField(null=True, blank=True, help_text="Présence d'une main courante d'escalier")
+    # ascenseur ou élévateur
+    ascenseur = models.BooleanField(null=True, blank=True, help="Présence d'un ascenseur ou d'un élévateur")
+
+
+class Label(model.Model):
+    nom = CharField(max_length=255)
+
+    def __str__(self):
+        return self.nom
+
+
+class Accessibilite(models.Model):
+    PERSONNELS_FORMES = "formés"
+    PERSONNELS_NON_FORMES = "non-formés"
+    PERSONNELS_CHOICES = [
+        (PERSONNELS_FORMES, "Personnels formés"),
+        (PERSONNELS_NON_FORMES, "Personnels non-formés"),
+    ]
+
+    # stationnement à proximité (simplification)
+    presence = models.BooleanField(null=True, blank=True)
+    reserve_pmr = models.BooleanField(null=True, blank=True)
+
+    # cheminements
+    cheminement_stationnement = models.ForeignKey(
+        Circulation, null=True, blank=True, help_text="Cheminement depuis le stationnement de l'ERP"
+    )
+    cheminement_stationnement_ext = models.ForeignKey(
+        Circulation, null=True, blank=True, help_text="Cheminement depuis le stationnement extérieur à l'ERP"
+    )
+    cheminement_place_entree = models.ForeignKey(
+        Circulation, null=True, blank=True, help_text="Cheminement du stationnement à l'entrée du bâtiment"
+    )
+    cheminement_parcelle_entree = models.ForeignKey(
+        Circulation,
+        null=True,
+        blank=True,
+        help_text="Cheminement depuis l'entrée de la parcelle de terrain à l'entrée du bâtiment",
+    )
+    cheminement_entree_accueil = models.ForeignKey(
+        Circulation, null=True, blank=True, help_text="Cheminement de l'entrée du bâtiment à l'accueil"
+    )
+
+    # entrées principale et secondaires
+    entree = models.ForeignKey(Circulation, null=True, blank=True, help_text="Caractéristiques de l'entrée")
+    entree_signaletique = models.BooleanField(
+        null=True, blank=True, help_text="Présence d'une signalétique matérialisant l'entrée"
+    )
+    entree_secondaire = models.BooleanField(null=True, blank=True, help_text="Présence d'une entrée secondaire")
+    entree_secondaire_informations = models.TextField(
+        max_length=500, null=True, blank=True, help_text="Précisions sur les modalités d'accès de l'entrée secondaire"
+    )
+
+    # accueil
+    accueil_personnels = models.CharField(
+        max_length=255, null=True, blank=True, choices=PERSONNELS_CHOICES, help_text="Présence et type de personnels d'accueil"
+    )
+    accueil_lsf = model.BooleanField(null=True, blank=True, help_text="Présence d'équipements LSF")
+    accueil_bim = model.BooleanField(null=True, blank=True, help_text="Présence d'équipements BIM")
+    accueil_prestations = model.TextField(
+        max_length=1000, null=True, blank=True, help_text="Description libre des prestations adaptées"
+    )
+
+    # sanitaires
+    sanitaires_presence = models.BooleanField(null=True, blank=True, help_text="Présence de sanitaires dans l'établissement")
+    sanitaires_adaptes = models.IntegerField(null=True, blank=True, help_text="Nombre de sanitaires adaptés dans l'établissement")
+
+    # labels
+    labels = models.ManyToManyField(Label)
+
+    def __str__(self):
+        return f"Accessibilité #{self.id}"
+
+
 class Erp(models.Model):
     nom = models.CharField(max_length=255, help_text="Nom de l’établissement ou de l’enseigne")
     activite = models.ForeignKey(Activite, null=True, blank=True, on_delete=models.SET_NULL)
@@ -31,6 +133,9 @@ class Erp(models.Model):
     # datetimes
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # accessibilité
+    # see https://docs.djangoproject.com/en/3.0/topics/db/examples/one_to_one/
+    accessibilite = models.OneToOneField(Accessibilite, null=True, blank=True, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return f"ERP #{self.id} ({self.nom})"
