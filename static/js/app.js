6f8d187b2b28c4c95603a349d22169693459b831
Nicolas Perriault <nicolas@perriault.net>
2020-03-01 14:26:47 +0100

    Add an address autocomplete widget.

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,19 @@
-let layers = [], markers, map;
+let layers = [],
+  markers,
+  map;
 
 // see https://leafletjs.com/examples/geojson/
 function onEachFeature(feature, layer) {
   const properties = feature.properties;
   const content = [
-    '<p><strong><a href="' + properties.absolute_url + '">' + properties.nom + '</a></strong>',
-      properties.activite__nom && ("<br>" + properties.activite__nom) || "",
-      '<br>' + properties.adresse,
-    '</p>'
+    '<p><strong><a href="',
+    properties.absolute_url,
+    '">',
+    properties.nom,
+    "</a></strong>",
+    (properties.activite__nom && "<br>" + properties.activite__nom) || "",
+    "<br>" + properties.adresse,
+    "</p>"
   ].join("");
   layer.bindPopup(content);
   layer.pk = parseInt(feature.properties.pk, 10);
@@ -23,13 +29,19 @@ function iconCreateFunction(cluster) {
 }
 
 function initMap(info, geoJson) {
-  const tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+  const tiles = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+  );
 
   const geoJsonLayer = L.geoJSON(geoJson, { onEachFeature: onEachFeature });
 
-  map = L.map("map").addLayer(tiles).setMinZoom(info.zoom - 2);
+  map = L.map("map")
+    .addLayer(tiles)
+    .setMinZoom(info.zoom - 2);
 
   markers = L.markerClusterGroup({
     disableClusteringAtZoom: 17,
@@ -38,17 +50,24 @@ function initMap(info, geoJson) {
   });
   markers.addLayer(geoJsonLayer);
   map.addLayer(markers);
-  map.fitBounds(markers.getBounds().pad(.1));
 
-  L.control.locate({
-    icon: "icon icon-street-view a4a-locate-icon",
-    strings: { title: "Localisez moi" }
-  }).addTo(map);
+  if (geoJson.features.length > 0) {
+    map.fitBounds(markers.getBounds().pad(0.1));
+  } else {
+    map.setView(info.center, info.zoom);
+  }
+
+  L.control
+    .locate({
+      icon: "icon icon-street-view a4a-locate-icon",
+      strings: { title: "Localisez moi" }
+    })
+    .addTo(map);
 }
 
 function openMarkerPopup(target) {
   if (!markers) {
-    console.warn("No marker clusters were registered, cannot open marker.")
+    console.warn("No marker clusters were registered, cannot open marker.");
     return;
   }
   layers.forEach(function(layer) {
@@ -68,5 +87,46 @@ window.addEventListener("DOMContentLoaded", function() {
       const pk = parseInt(link.dataset.erpId, 10);
       if (pk) openMarkerPopup(pk);
     });
+  });
+
+  $("#q").autocomplete({
+    dataType: "json",
+    deferRequestBy: 350,
+    minChars: 2,
+    paramName: "q",
+    preserveInput: true,
+    serviceUrl: "https://api-adresse.data.gouv.fr/search",
+    params: {
+      type: "street",
+      lat: $("#q").data("lat"),
+      lon: $("#q").data("lon")
+    },
+    onSearchStart: function(params) {
+      // ensure city is always passed to query
+      params.q += ", " + $("#q").data("commune");
+      return params;
+    },
+    onSelect: function(suggestion) {
+      map.setView([suggestion.data[1], suggestion.data[0]]).setZoom(17);
+    },
+    transformResult: function(response) {
+      return {
+        suggestions: response.features
+          .filter(function(feature) {
+            return (
+              feature.properties.city.toLowerCase() ===
+              $("#q")
+                .data("commune")
+                .toLowerCase()
+            );
+          })
+          .map(function(feature) {
+            return {
+              value: feature.properties.label,
+              data: feature.geometry.coordinates
+            };
+          })
+      };
+    }
   });
 });
