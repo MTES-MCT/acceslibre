902fd16107db3341f7ce95a66ca64dd671a89d27
Nicolas Perriault <nicolas@perriault.net>
2020-04-06 08:23:59 +0200

    Add swagger api docs.

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework import routers
 from .views import AccessibiliteViewSet, ActiviteViewSet, ErpViewSet
 
@@ -9,6 +10,14 @@ router.register(r"erps", ErpViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
     path(
         "api-auth/", include("rest_framework.urls", namespace="rest_framework")
     ),
