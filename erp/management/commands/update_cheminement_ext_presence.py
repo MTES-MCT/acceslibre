08189e8ed7f3cff3bc3fa267547473340be70ea3
Nicolas Perriault <nicolas@perriault.net>
2020-04-23 10:24:32 +0200

    Add update_cheminement_ext_presence command.

---
+++ erp/management/commands/update_cheminement_ext_presence.py
@@ -0,0 +1,34 @@
+import random
+import string
+import unicodedata
+
+from erp.models import Accessibilite
+from django.core.management.base import BaseCommand, CommandError
+
+fields = [
+    "cheminement_ext_plain_pied",
+    "cheminement_ext_nombre_marches",
+    "cheminement_ext_reperage_marches",
+    "cheminement_ext_main_courante",
+    "cheminement_ext_rampe",
+    "cheminement_ext_ascenseur",
+    "cheminement_ext_pente",
+    "cheminement_ext_devers",
+    "cheminement_ext_bande_guidage",
+    "cheminement_ext_guidage_sonore",
+    "cheminement_ext_retrecissement",
+]
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        lst = Accessibilite.objects.all()
+        for accessibilite in lst:
+            filled = any(getattr(accessibilite, f) is not None for f in fields)
+            if filled and accessibilite.cheminement_ext_presence is not True:
+                accessibilite.cheminement_ext_presence = True
+                accessibilite.save()
+                print(f"Updated cheminement_ext_presence for {accessibilite.erp}.")
