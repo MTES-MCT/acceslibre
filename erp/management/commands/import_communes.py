a788736001734dfca0ab1b8764c2829f33a6acd2
Nicolas Perriault <nicolas@perriault.net>
2020-05-06 13:16:08 +0200

    Improve commune data quality.

--- erp/management/commands/import_communes.py
+++ erp/management/commands/import_communes.py
@@ -1,13 +1,20 @@
 import csv
 import os
-import re
+import sys
+import time
 
 from django.contrib.gis.geos import Point
 from django.core.management.base import BaseCommand, CommandError
 
 from erp.models import Commune
+from erp.geocoder import geocode_commune
 
-# TODO: arg pour telecharger depuis https://sql.sh/ressources/sql-villes-france/villes_france.csv
+# Le fichier source est téléchargeable à https://sql.sh/ressources/sql-villes-france/villes_france.csv
+# Il faut l'enregistrer dans le répertoire `data` à la racine du dépôt.
+
+
+class SkipImport(Exception):
+    pass
 
 
 class Command(BaseCommand):
@@ -22,6 +29,11 @@ class Command(BaseCommand):
         return os.path.join(os.path.dirname(here), "data", "villes_france.csv",)
 
     def import_row(self, row):
+        # TODO: check if exists
+        code_insee = row[10]
+        assert len(code_insee) == 5, code_insee
+        if Commune.objects.filter(code_insee=code_insee).count() > 0:
+            raise SkipImport("SKIP")
         departement = row[1]
         assert len(departement) >= 2 and len(departement) <= 3, departement
         nom = row[5]
@@ -32,37 +44,71 @@ class Command(BaseCommand):
         else:
             code_postaux = [raw_code_postal]
         assert all(len(cp) == 5 for cp in code_postaux), code_postaux
-        code_insee = row[10]
-        assert len(code_insee) == 5, code_insee
-        superficie = int(float(row[18]) * 1000)
+        superficie = int(float(row[18]) * 100)  # hectares
         lon = float(row[19])
         lat = float(row[20])
         assert superficie != 0, (nom, superficie)
-        return Commune(
-            departement=departement,
-            nom=nom,
-            code_insee=code_insee,
-            superficie=superficie,
-            geom=Point(lon, lat, srid=4326),
-            code_postaux=code_postaux,
-        )
+        data = geocode_commune(code_insee)
+        time.sleep(0.02)
+        try:
+            if data:
+                departement = data["departement"]["code"]
+                nom = data["nom"]
+                superficie = data["surface"]  # hectares
+                lon = data["centre"]["coordinates"][0]
+                lat = data["centre"]["coordinates"][1]
+                code_postaux = data["codesPostaux"]
+            commune = Commune(
+                departement=departement,
+                nom=nom,
+                code_insee=code_insee,
+                superficie=superficie,  # hectares
+                geom=Point(lon, lat, srid=4326,),
+                code_postaux=code_postaux,
+            )
+        except KeyError as err:
+            raise RuntimeError(f"{nom} ({departement}): Key error: {err}")
+        except (AttributeError, IndexError, TypeError, ValueError) as err:
+            raise RuntimeError(f"{nom} ({departement}): {err}")
+        commune.save()
 
     def handle(self, *args, **options):
+        self.errors = []
         csv_path = self.get_csv_path()
         self.stdout.write(f"Importation des communes depuis {csv_path}")
-        to_import = []
 
         with open(csv_path, "r") as file:
             reader = csv.reader(file)
             try:
                 for row in reader:
-                    commune = self.import_row(row)
-                    if commune is not None:
-                        to_import.append(commune)
+                    try:
+                        self.import_row(row)
+                    except AssertionError as err:
+                        self.errors.append(
+                            f"Validation error: line {reader.line_num}: {err}"
+                        )
+                        sys.stdout.write("E")
+                        sys.stdout.flush()
+                    except RuntimeError as err:
+                        self.errors.append(
+                            f"Import error: line {reader.line_num}: {err}"
+                        )
+                        sys.stdout.write("E")
+                        sys.stdout.flush()
+                    except SkipImport as err:
+                        sys.stdout.write("S")
+                        sys.stdout.flush()
+                    else:
+                        sys.stdout.write(".")
+                        sys.stdout.flush()
             except csv.Error as err:
-                sys.exit(f"file {filename}, line {reader.line_num}: {err}")
-        if len(to_import) == 0:
-            print("Rien à importer.")
-            exit(0)
-        res = Commune.objects.bulk_create(to_import)
+                self.errors.append(f"CSV error: line {reader.line_num}: {err}")
+                sys.stdout.write("E")
+                sys.stdout.flush()
+
+        if len(self.errors) != 0:
+            print(f"{len(self.errors)} errors have been encountered:")
+            for err in self.errors:
+                print(f"- {err}")
+            exit(1)
         print("Importation effectuée.")
