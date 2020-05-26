662b273552b3bc36f033dbc2c7d3b36fbbf95464
Nicolas Perriault <nicolas@perriault.net>
2020-05-26 12:21:56 +0200

    Add splinter browser tests. (#20)

---
+++ erp/test/test_func.py
@@ -0,0 +1,37 @@
+import pytest
+
+from django.urls import reverse
+from splinter import Browser
+
+from .fixtures import data
+from erp.models import Erp
+from erp import schema
+
+
+@pytest.fixture
+def browser():
+    return Browser("django")
+
+
+def test_home(data, browser, capsys):
+    browser.visit(reverse("home"))
+
+    assert browser.title == "Accueil | Access4all"
+    assert len(browser.find_by_css("#home-communes-list .card")) == 1
+
+    assert data.erp.geom is not None
+    assert data.erp.published == True
+    assert data.erp.accessibilite is not None
+    assert len(browser.find_by_css("#home-latest-erps-list a")) == 1
+
+
+def test_erp_details(data, browser):
+    browser.visit(data.erp.get_absolute_url())
+
+    assert browser.title == "Aux bons croissants | Boulangerie | Jacou | Access4all"
+    assert browser.is_text_present(data.erp.nom)
+    assert browser.is_text_present(data.erp.activite.nom)
+    assert browser.is_text_present(data.erp.adresse)
+    assert browser.is_text_present("Sanitaires")
+    assert browser.is_text_present(schema.get_help_text("sanitaires_presence"))
+    assert browser.is_text_present(schema.get_help_text("sanitaires_adaptes"))
