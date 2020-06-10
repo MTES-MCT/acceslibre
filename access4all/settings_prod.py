40218ad4e575a499c7c9eec6ad7f0ac79de398c8
Nicolas Perriault <nicolas@perriault.net>
2020-06-10 17:08:06 +0200

    Allow clicking on add an erp button publicly.

--- access4all/settings_prod.py
+++ access4all/settings_prod.py
@@ -3,6 +3,7 @@ from .settings import *
 
 DEBUG = False
 
+# FIXME: this should eventually be provided by some env var
 ALLOWED_HOSTS = [
     "access4all.osc-fr1.scalingo.io",
     "access4all.beta.gouv.fr",
