e22482498cd2a007349b85b633251fbf66248ac5
Nicolas Perriault <nicolas@perriault.net>
2020-06-30 15:33:57 +0200

    Migrate CSS to Sass, improve a11y contrasts.

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "access4all.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "access4all.settings_prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
