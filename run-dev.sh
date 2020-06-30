551a488b95245459fd53f38e4152aa5b61c58172
Nicolas Perriault <nicolas@perriault.net>
2020-06-30 18:24:25 +0200

    Improve legibility of a11y field pills.

--- run-dev.sh
+++ run-dev.sh
@@ -3,6 +3,9 @@
 # Run Django dev server along sass watch daemon in parallel, handling
 # ctrl-c termination. Inspired by https://unix.stackexchange.com/a/478574.
 
+SOURCE_SCSS="static/scss/style.scss"
+TARGET_CSS="static/css/style.css"
+
 function ctrl_c() {
   echo "Shutting down development servers"
   kill -TERM "${runserver_pid}" > /dev/null 2>&1
@@ -15,7 +18,7 @@ trap ctrl_c INT
 
 python manage.py runserver --insecure &
 runserver_pid=$!
-python manage.py sass static/scss static/css --watch -t compressed &
+python manage.py sass $SOURCE_SCSS $TARGET_CSS --watch -t compressed &
 sasswatch_pid=$!
 
 wait ${runserver_pid} > /dev/null 2>&1
