3670043f2abc484065b1927f6c89f5a674a2f062
Nicolas Perriault <nicolas@perriault.net>
2020-07-02 14:29:30 +0200

    Rename access4all module to core. (#30)

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from django.db import connection
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     # Vérifie qu'on emploie bien les settings de test
-    assert os.environ.get("DJANGO_SETTINGS_MODULE") == "access4all.settings_test"
+    assert os.environ.get("DJANGO_SETTINGS_MODULE") == "core.settings_test"
 
     # Installe les extensions postgres pour la suite de test pytest
     with django_db_blocker.unblock(), connection.cursor() as cursor:
