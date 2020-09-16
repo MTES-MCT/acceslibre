641bedee9d1292d34026fe9ea3ae866151723190
Nicolas Perriault <nicolas@perriault.net>
2020-09-16 09:53:57 +0200

    Improve restore_db_file.sh script.

--- bin/restore_db_file.sh
+++ bin/restore_db_file.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-sudo -u postgres dropdb access4all
-sudo -u postgres createdb access4all
-pg_restore --clean --if-exists --no-owner --no-privileges \
-  --dbname "postgres://access4all:access4all@localhost/access4all" \
-  $1
+if [ $# -eq 0 ]
+  then
+    echo "Le chemin vers le fichier .pgsql est requis."
+    exit 1
+fi
 
+sudo echo "Recréation et import de la base $DATABASE_URL depuis $DUMP"
+read -r -p "Voulez-vous procéder (la base actuelle sera détruite) ? [o/N] " response
+
+case "$response" in
+    [oO][eE][sS]|[oO])
+        sudo -i -u postgres dropdb access4all
+        sudo -i -u postgres createdb access4all
+        pg_restore --clean --if-exists --no-owner --no-privileges --dbname $DATABASE_URL $1
+        ;;
+    *)
+        echo "Annulé."
+        ;;
+esac
