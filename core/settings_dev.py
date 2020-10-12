ed82387f9404f2ca05fc4d7cd20649544176400d
Nicolas Perriault <nicolas@perriault.net>
2020-10-12 23:37:19 +0200

    Clean up public_erp types list.

--- core/settings_dev.py
+++ core/settings_dev.py
@@ -42,5 +42,5 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {"console": {"class": "logging.StreamHandler",},},
-    "root": {"handlers": ["console"], "level": "DEBUG",},
+    "root": {"handlers": ["console"], "level": "INFO",},
 }
---
+++ erp/management/commands/public_erp_types_stats.py
@@ -0,0 +1,30 @@
+import time
+
+from pprint import pprint
+
+from django.core.management.base import BaseCommand
+
+from erp import public_erp
+
+
+class Command(BaseCommand):
+    help = "Liste les types d'établissements possédant des entrées dans l'API des établissements publics"
+
+    def handle(self, *args, **options):
+        good = []
+        final = {}
+        for erp_type in public_erp.TYPES:
+            try:
+                res = public_erp.get(f"{public_erp.BASE_URL}/organismes/{erp_type}")
+                count = len(res["features"][0])
+                if count > 0:
+                    good.append(erp_type)
+                    print(f"PASS {erp_type} ({count})")
+            except RuntimeError as err:
+                print(f"SKIP error on {res.url}: {err}")
+            time.sleep(0.02)
+        if len(good) > 0:
+            for erp_type in good:
+                final[erp_type] = public_erp.TYPES[erp_type]
+        print("Final dict:")
+        pprint(final)
