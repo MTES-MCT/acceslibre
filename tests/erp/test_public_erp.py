43b6b90db84bb0e5f2591a7ba1e8e42260b8457d
Nicolas Perriault <nicolas@perriault.net>
2020-10-12 15:38:08 +0200

    Prefill a few erp categories.

--- tests/erp/test_public_erp.py
+++ tests/erp/test_public_erp.py
@@ -8,6 +8,7 @@ from tests.fixtures import data
 
 def test_extract_numero_voie():
     cases = [
+        ("", (None, "")),
         ("4 grand rue", ("4", "grand rue")),
         ("grand rue", (None, "grand rue")),
         ("4TER grand rue", ("4TER", "grand rue")),
@@ -49,7 +50,7 @@ def test_parse_feature_jacou(data):
         "actif": True,
         "coordonnees": [3.9106014, 43.6609939],
         "naf": None,
-        "activite": data.administration_publique.pk,
+        "activite": data.mairie.pk,
         "nom": "Mairie - Jacou",
         "siret": None,
         "numero": "4",
@@ -61,6 +62,72 @@ def test_parse_feature_jacou(data):
         "contact_email": "mairie@ville-jacou.fr",
         "telephone": "04 67 55 88 55",
         "site_internet": "http://www.ville-jacou.fr",
+    }
+
+
+def test_parse_feature_gendarmerie(data):
+    json_feature = json.loads(
+        """
+        {
+            "type":"Feature",
+            "geometry":{
+                "type":"Point",
+                "coordinates":[
+                    3.91375272,
+                    43.64536644
+                ]
+            },
+            "properties":{
+                "id":"gendarmerie-34057-01",
+                "codeInsee":"34057",
+                "pivotLocal":"gendarmerie",
+                "nom":"Brigade de gendarmerie - Castelnau-le-Lez",
+                "adresses":[
+                    {
+                        "type":"géopostale",
+                        "lignes":[
+                            "635 Avenue de la Monnaie"
+                        ],
+                        "codePostal":"34170",
+                        "commune":"Castelnau-le-Lez",
+                        "coordonnees":[
+                            3.91375272,
+                            43.64536644
+                        ]
+                    }
+                ],
+                "email":"https://www.contacterlagendarmerie.fr",
+                "telephone":"04 99 74 29 50",
+                "url":"http://www.gendarmerie.interieur.gouv.fr",
+                "zonage":{
+                    "communes":[
+                        "34169 Montferrier-sur-Lez",
+                        "34057 Castelnau-le-Lez",
+                        "34120 Jacou",
+                        "34077 Clapiers"
+                    ]
+                }
+            }
+        }"""
+    )
+    assert public_erp.parse_feature(json_feature) == {
+        "source": "public_erp",
+        "source_id": "gendarmerie-34057-01",
+        "actif": True,
+        "coordonnees": [3.91375272, 43.64536644],
+        "naf": None,
+        "activite": data.administration_publique.pk,
+        "nom": "Brigade de gendarmerie - Castelnau-le-Lez",
+        "siret": None,
+        "numero": "635",
+        "voie": "Avenue de la Monnaie",
+        "lieu_dit": None,
+        "code_postal": "34170",
+        "commune": "Castelnau-le-Lez",
+        "code_insee": "34057",
+        "contact_email": None,
+        "telephone": "04 99 74 29 50",
+        "site_internet": "http://www.gendarmerie.interieur.gouv.fr",
     }
 
 
@@ -109,7 +176,7 @@ def test_parse_feature_montreuil(data):
         "actif": True,
         "coordonnees": [2.441878, 48.860395],
         "naf": None,
-        "activite": data.administration_publique.pk,
+        "activite": data.mairie.pk,
         "nom": "Mairie - Montreuil",
         "siret": None,
         "numero": "1",
