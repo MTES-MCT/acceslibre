c18d69e89fcb02f1ab8636cedb38f50fdbadbfe8
Nicolas Perriault <nicolas@perriault.net>
2020-10-20 22:02:53 +0200

    Update outdated makegraphs script.

--- makegraphs.sh
+++ makegraphs.sh
@@ -6,5 +6,4 @@ mkdir -p graphs
 ./manage.py graph_models --hide-edge-labels -n -I Accessibilite -o graphs/Accessibilite.png
 ./manage.py graph_models --hide-edge-labels -n -I Activite -o graphs/Activite.png
 ./manage.py graph_models --hide-edge-labels -n -I Erp -o graphs/Erp.png
-./manage.py graph_models --hide-edge-labels -n -I EquipementMalentendant -o graphs/EquipementMalentendant.png
 ./manage.py graph_models --hide-edge-labels -n -I Label -o graphs/Label.png
