94f928b6373792ad5671706913a79be59d13e01e
Nicolas Perriault <nicolas@perriault.net>
2020-12-15 17:32:02 +0100

    Add subscription admin.

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from subscription.models import ErpSubscription
+
+
+@admin.register(ErpSubscription)
+class ErpSubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_erp",
+        "user",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = [
+        "created_at",
+    ]
+    # list_display_links = ("created_at",)
+    list_select_related = ("erp", "erp__activite", "user")
+    ordering = ("-created_at",)
+    search_fields = ("erp__nom", "user__username")
+    autocomplete_fields = ["erp", "user"]
+
+    def get_erp(self, obj):
+        if obj.erp:
+            return mark_safe(
+                f'<a href="{obj.erp.get_absolute_url()}" target="_blank">{obj.erp}</a>'
+            )
+        return ""
+
+    get_erp.short_description = "ERP"
