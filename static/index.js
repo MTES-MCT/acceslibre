c6f12979ba286909d2b26cb2b3c7c34ab1c59c19
Calev Eliacheff <c.eliacheff@gmail.com>
2021-01-12 12:27:02 +0100

    Babel + browserlist + polyfills + remove dist files

--- static/index.js
+++ static/index.js
@@ -1,3 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import jquery from 'jquery';
 window.$ = window.jQuery = jquery;
 import 'devbridge-autocomplete';
@@ -9,9 +12,12 @@ import 'leaflet-center-cross';
 import 'chart.js';
 import 'sentry';
 
+import './js/app';
+
 import 'leaflet/dist/leaflet.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css'
 import './scss/style.scss';
+import './icons/styles.css'
 
 console.log("hello world!");
