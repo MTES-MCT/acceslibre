8ac9ea85a88045c8aaf27378bdb1e6e656f67eb4
Nicolas Perriault <nicolas@perriault.net>
2021-01-12 15:23:45 +0100

    Deduplicate erp search results.

--- tests/erp/provider/test_search.py
+++ tests/erp/provider/test_search.py
@@ -7,15 +7,21 @@ def test_search_sort_and_filter():
     results = search.sort_and_filter_results(
         "34830",
         [
-            {"code_insee": "42000", "coordonnees": [1, 1]},
-            {"code_insee": "34830", "coordonnees": [1, 1]},
-            {"code_insee": "11000", "coordonnees": None},
-            {"code_insee": "34170", "coordonnees": [1, 1]},
-            {"code_insee": "34270", "coordonnees": None},
+            {"siret": "1", "code_insee": "42000", "coordonnees": [1, 1]},
+            {"siret": "2", "code_insee": "34830", "coordonnees": [1, 1]},
+            {"siret": "2", "code_insee": "34830", "coordonnees": [1, 1]},
+            {"siret": "3", "code_insee": "11000", "coordonnees": None},
+            {"siret": "4", "code_insee": "34170", "coordonnees": [1, 1]},
+            {"siret": "5", "code_insee": "34270", "coordonnees": None},
+            {"siret": None, "code_insee": "34830", "coordonnees": [1, 1]},
         ],
     )
 
     assert results == [
-        {"code_insee": "34830", "coordonnees": [1, 1]},
-        {"code_insee": "34170", "coordonnees": [1, 1]},
+        # duplicate not included
+        {"siret": "2", "code_insee": "34830", "coordonnees": [1, 1]},
+        # erp can have missing siret
+        {"siret": None, "code_insee": "34830", "coordonnees": [1, 1]},
+        # erp in same departement included
+        {"siret": "4", "code_insee": "34170", "coordonnees": [1, 1]},
     ]
