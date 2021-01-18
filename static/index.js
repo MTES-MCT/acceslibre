b5bcb72ee9cd13adbe5287fb09d526eb18601e7c
Calev Eliacheff <c.eliacheff@gmail.com>
2021-01-18 09:16:28 +0100

    working libs

--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+// https://stackoverflow.com/a/47984928
 import './js/jquery';
 
 import 'devbridge-autocomplete';
@@ -12,9 +13,12 @@ import 'chart.js';
 import 'sentry';
 
 import('./js/app').then();
-import './vendor/selectWoo-1.0.8/js/select2.min.js'
-import './vendor/selectWoo-1.0.8/js/i18n/fr.js'
 
+// https://stackoverflow.com/a/49722514
+import('./vendor/selectWoo-1.0.8/js/select2.full.min').then((select2) => {
+    select2($);
+    import('./vendor/selectWoo-1.0.8/js/i18n/fr.js').then();
+})
 
 import './icons/styles.css'
 import 'leaflet/dist/leaflet.css'
