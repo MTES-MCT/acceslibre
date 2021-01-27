800f7ec8f52ced68405c9b7c9ad7681dfef668c8
Nicolas Perriault <nicolas@perriault.net>
2021-01-27 09:31:44 +0100

    Add a right click context menu to add erp from map.

--- static/js/app.js
+++ static/js/app.js
@@ -27,7 +27,7 @@ window.a4a = (function () {
     const iconName = features.activite__vector_icon || "building";
     const size = highlight ? 48 : 32;
     const options = {
-      iconUrl: "/static/img/mapicons.svg#" + iconName,
+      iconUrl: `/static/img/mapicons.svg#${iconName}`,
       iconSize: [size, size],
       iconAnchor: [size / 2, size],
       popupAnchor: [0, -size],
@@ -41,33 +41,33 @@ window.a4a = (function () {
   }
 
   // see https://leafletjs.com/examples/geojson/
-  function onEachFeature(feature, layer) {
-    const properties = feature.properties;
-    const content = [
-      '<div class="a4a-map-popup-content"><strong><a class="text-primary" href="',
-      properties.absolute_url,
-      '">',
-      properties.nom,
-      "</a></strong>",
-      (properties.activite__nom && "<br>" + properties.activite__nom) || "",
-      "<br>" + properties.adresse,
-      '<br><a href="' + properties.contrib_localisation_url + '">',
-      '<i aria-hidden="true" class="icon icon-pencil a4a-icon-small-top"></i>',
-      " Affiner la localisation</a>",
-      "</div>",
-    ].join("");
-    layer.bindPopup(content);
-    layer.pk = parseInt(feature.properties.pk, 10);
+  function onEachFeature({ properties: props }, layer) {
+    layer.bindPopup(`
+      <div class="a4a-map-popup-content">
+        <strong>
+          <a class="text-primary" href="${props.absolute_url}">${props.nom}</a>
+        </strong>
+        ${(props.activite__nom && "<br>" + props.activite__nom) || ""}
+        <br>${props.adresse}
+        <br>
+        <a href="${props.contrib_localisation_url}">
+          <i aria-hidden="true" class="icon icon-pencil a4a-icon-small-top"></i>
+          Affiner la localisation
+        </a>
+      </div>`);
+    layer.pk = parseInt(props.pk, 10);
     layers.push(layer);
   }
 
-  function pointToLayer(feature, coords) {
-    const props = feature.properties;
+  function pointToLayer({ properties: props }, coords) {
     return L.marker(coords, {
       alt: props.nom,
       title:
         (props.activite__nom ? props.activite__nom + ": " : "") + props.nom,
-      icon: createIcon(currentPk && Number(props.pk) === currentPk, props),
+      icon: createIcon(
+        currentPk && Number(props.pk) === currentPk,
+        props.activite__vector_icon
+      ),
     });
   }
 
@@ -84,11 +84,10 @@ window.a4a = (function () {
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
         id: styleId,
-        attribution: [
-          'Cartographie &copy; contributeurs <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-          'Imagerie © <a href="https://www.mapbox.com/">Mapbox</a>',
-        ].join(", "),
+        attribution: `
+          Cartographie &copy; contributeurs <a href="https://www.openstreetmap.org/">OpenStreetMap</a>
+          <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>
+          Imagerie © <a href="https://www.mapbox.com/">Mapbox</a>`,
         maxZoom: 20,
         tileSize: 512,
         zoomOffset: -1,
@@ -121,11 +120,71 @@ window.a4a = (function () {
     return satelliteTiles;
   }
 
-  function createMap(id, options) {
-    options = options || {};
+  function safeBase64Encode(data) {
+    return btoa(unescape(encodeURIComponent(JSON.stringify(data))));
+  }
+
+  function onMapContextMenu({ latlng, target: map }) {
+    // prevent imprecise locations by requiring a minimum zoom level
+    if (map.getZoom() < 16) {
+      return;
+    }
+
+    const popup = L.popup()
+      .setLatLng(latlng)
+      .setContent(
+        '<a href="#" class="a4a-map-add">Ajouter un établissement ici</a>'
+      )
+      .openOn(map);
+
+    $(".a4a-map-add").on("click", async function (event) {
+      event.preventDefault();
+      const { lat, lng } = latlng;
+      const req = await fetch(
+        `https://api-adresse.data.gouv.fr/reverse/?lon=${lng}&lat=${lat}`
+      );
+      const res = await req.json();
+      const results = res.features.filter(
+        ({ properties }) => properties.type == "housenumber"
+      );
+      if (results.length == 0) {
+        $(this).replaceWith("Aucune adresse ne correspond à cet emplacement.");
+        return;
+      }
+      const adresses = results.map(({ properties, geometry }) => {
+        return {
+          label: properties.label,
+          data: safeBase64Encode({
+            source: "public",
+            source_id: `ban:${properties.id}`,
+            actif: true,
+            coordonnees: [geometry.coordinates[0], geometry.coordinates[1]],
+            naf: null,
+            activite: null,
+            nom: null,
+            siret: null,
+            numero: properties.housenumber,
+            voie: properties.street,
+            lieu_dit: properties.locality,
+            code_postal: properties.postcode,
+            commune: properties.city,
+            code_insee: properties.citycode,
+          }),
+        };
+      }, []);
+      $(this).replaceWith(`
+        <p><b>Choisissez une adresse :</b></p>
+        <ul class="a4a-map-reverse-results">
+          ${adresses.map(({ data, label }) => {
+            return `<li><a href="/contrib/admin-infos/?data=${data}">${label}</a></li>`;
+          })}
+        </ul>`);
+    });
+  }
+
+  function createMap(id, options = {}) {
     const defaults = { layers: [getStreetsTiles()] };
-    const _options = Object.assign({}, defaults, options);
-    const map = L.map(id, _options);
+    const map = L.map(id, { ...defaults, options });
     L.control
       .layers({
         "Plan des rues": getStreetsTiles(),
@@ -137,11 +196,16 @@ window.a4a = (function () {
 
   function initAppMap(info, pk, around, geoJson) {
     currentPk = pk;
+
     const geoJsonLayer = L.geoJSON(geoJson, {
       onEachFeature: onEachFeature,
       pointToLayer: pointToLayer,
     });
+
     map = createMap("app-map").setMinZoom(info.zoom - 2);
+
+    // right-click menu
+    map.on("contextmenu", onMapContextMenu);
 
     // markers
     markers = L.markerClusterGroup({
@@ -186,126 +250,115 @@ window.a4a = (function () {
       console.warn("No marker clusters were registered, cannot open marker.");
       return;
     }
-    layers.forEach(function (layer) {
+    layers.forEach((layer) => {
       if (layer.pk === pk) {
-        markers.zoomToShowLayer(layer, function () {
+        markers.zoomToShowLayer(layer, () => {
           layer.openPopup();
         });
       }
     });
   }
 
-  window.addEventListener("DOMContentLoaded", function () {
-    if (window.hasOwnProperty("$")) {
-      [].forEach.call(
-        document.querySelectorAll(".a4a-geo-link"),
-        function (link) {
-          link.addEventListener("click", function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            const pk = parseInt(link.dataset.erpId, 10);
-            if (pk) openMarkerPopup(pk);
-          });
-        }
-      );
-
-      // Django crispy forms asterisk a11y improvements
-      $(".asteriskField").each(function (i, elem) {
-        $(elem).replaceWith(
-          '&nbsp;<small>(requis)</small><abbr class="asteriskField" title="(obligatoire)">*</abbr>'
-        );
+  window.addEventListener("DOMContentLoaded", () => {
+    [].forEach.call(document.querySelectorAll(".a4a-geo-link"), (link) => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const pk = parseInt(link.dataset.erpId, 10);
+        if (pk) openMarkerPopup(pk);
       });
+    });
 
-      // Autocomplete
-      $("#q").autocomplete({
-        deferRequestBy: 100,
-        minChars: 2,
-        lookup: function (query, done) {
-          const $input = $("#q");
-          const commune = $input.data("commune");
-          const communeSlug = $input.data("commune-slug");
-          const lat = $input.data("lat");
-          const lon = $input.data("lon");
-          const results = {};
-          const streetsReq = $.ajax({
-            url: "https://api-adresse.data.gouv.fr/search",
-            data: {
-              q: query + (commune ? ", " + commune : ""),
-              type: "street",
-              lat: lat,
-              lon: lon,
-              citycode: $input.data("code-insee"),
-            },
-          })
-            .then(function (result) {
-              return result.features
-                .filter(function (feature) {
-                  return (
-                    (commune &&
-                      feature.properties.city.toLowerCase() ===
-                        commune.toLowerCase()) ||
-                    true
-                  );
-                })
-                .map(function (feature) {
-                  return {
-                    value: feature.properties.label,
-                    data: {
-                      type: "adr",
-                      score: feature.properties.score,
-                      url:
-                        "/app/" +
-                        communeSlug +
-                        "/?around=" +
-                        feature.geometry.coordinates[1] +
-                        "," +
-                        feature.geometry.coordinates[0],
-                    },
-                  };
-                });
-            })
-            .fail(function (err) {
-              console.error(err);
-            });
-          const erpsReq = $.ajax({
-            url: "/app/autocomplete/",
-            dataType: "json",
-            data: { q: query, commune_slug: communeSlug },
-          })
-            .then(function (result) {
-              return result.suggestions.map(function (sugg) {
+    // Django crispy forms asterisk a11y improvements
+    $(".asteriskField").each((i, elem) => {
+      $(elem).replaceWith(
+        '&nbsp;<small>(requis)</small><abbr class="asteriskField" title="(obligatoire)">*</abbr>'
+      );
+    });
+
+    // Autocomplete
+    $("#q").autocomplete({
+      deferRequestBy: 100,
+      minChars: 2,
+      lookup: function (query, done) {
+        const $input = $("#q");
+        const commune = $input.data("commune");
+        const communeSlug = $input.data("commune-slug");
+        const lat = $input.data("lat");
+        const lon = $input.data("lon");
+        const results = {};
+        const streetsReq = $.ajax({
+          url: "https://api-adresse.data.gouv.fr/search",
+          data: {
+            q: query + (commune ? ", " + commune : ""),
+            type: "street",
+            lat: lat,
+            lon: lon,
+            citycode: $input.data("code-insee"),
+          },
+        })
+          .then(function ({ features }) {
+            return features
+              .filter(({ properties: { city } }) => {
+                return (
+                  (commune && city.toLowerCase() === commune.toLowerCase()) ||
+                  true
+                );
+              })
+              .map(function ({
+                properties: { label, score },
+                geometry: { coordinates },
+              }) {
                 return {
-                  value: sugg.value,
+                  value: label,
                   data: {
-                    type: "erp",
-                    score: sugg.data.score,
-                    url: sugg.data.url,
+                    type: "adr",
+                    score,
+                    url: `/app/${communeSlug}/?around=${coordinates[1]},${coordinates[0]}`,
                   },
                 };
               });
-            })
-            .fail(function (err) {
-              console.error(err);
+          })
+          .fail((err) => {
+            console.error(err);
+          });
+
+        const erpsReq = $.ajax({
+          url: "/app/autocomplete/",
+          dataType: "json",
+          data: { q: query, commune_slug: communeSlug },
+        })
+          .then((result) => {
+            return result.suggestions.map(function (sugg) {
+              return {
+                value: sugg.value,
+                data: {
+                  type: "erp",
+                  score: sugg.data.score,
+                  url: sugg.data.url,
+                },
+              };
             });
-          $.when(streetsReq, erpsReq)
-            .done(function (streets, erps) {
-              const results = [].sort.call(
-                streets.concat(erps),
-                function (a, b) {
-                  return a.data.score - b.data.score;
-                }
-              );
-              done({ suggestions: results });
-            })
-            .fail(function (err) {
-              console.error(err);
+          })
+          .fail((err) => {
+            console.error(err);
+          });
+        $.when(streetsReq, erpsReq)
+          .done((streets, erps) => {
+            done({
+              suggestions: [].sort.call(streets.concat(erps), (a, b) => {
+                return a.data.score - b.data.score;
+              }),
             });
-        },
-        onSelect: function (suggestion) {
-          document.location = suggestion.data.url;
-        },
-      });
-    }
+          })
+          .fail((err) => {
+            console.error(err);
+          });
+      },
+      onSelect: (suggestion) => {
+        document.location = suggestion.data.url;
+      },
+    });
   });
 
   return {
