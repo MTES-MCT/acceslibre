85c46fa7e0a586ebf0b4c730c9ff20cc90f994c5
Calev Eliacheff <c.eliacheff@gmail.com>
2021-03-03 21:46:56 +0100

    extract logic, add email report

---
+++ erp/jobs/import_centres_vaccination.py
@@ -0,0 +1,94 @@
+"Imports vaccination centers from open data: https://www.data.gouv.fr/fr/datasets/lieux-de-vaccination-contre-la-covid-19/"
+
+import requests
+
+from django.conf import settings
+
+from core import mailer
+from erp.mapper import RecordMapper
+from erp.models import Activite
+
+
+API_URL = "https://www.data.gouv.fr/api/1/datasets/lieux-de-vaccination-contre-la-covid-19/"
+
+def job(dataset_url='', verbose=False, report=False):
+    if not dataset_url:
+        dataset_url = _retrieve_latest_dataset_url()
+
+    json_data = _retrieve_json_data(dataset_url)
+
+    activite = Activite.objects.filter(slug="centre-de-vaccination").first()
+    if not activite:
+        raise RuntimeError("L'activité Centre de vaccination n'existe pas.")
+
+    errors = []
+    imported = 0
+    skipped = 0
+
+    if "features" not in json_data:
+        raise RuntimeError("Liste des centres manquante")
+
+    for record in json_data["features"]:
+        try:
+            erp = RecordMapper(record).process(activite)
+            if verbose:
+                print(f"- {erp.nom}\n  {erp.code_postal} {erp.commune_ext.nom}")
+            else:
+                print(".", end="", flush=True)
+            imported += 1
+        except RuntimeError as err:
+            if not verbose:
+                print("S", end="", flush=True)
+            errors.append(err)
+            skipped += 1
+
+    if verbose and len(errors) > 0:
+        print("Erreurs rencontrées :")
+        for error in errors:
+            print(f"- {error}")
+
+    print("Opération effectuée:")
+    print(f"- {imported} centres importés")
+    print(f"- {skipped} écartés")
+
+    _send_report(errors, imported, skipped)
+
+def _get_json(url):
+    try:
+        return requests.get(url).json()
+    except requests.exceptions.RequestException as err:
+        raise RuntimeError(
+            f"Erreur de récupération des données JSON: {url}:\n  {err}"
+        )
+
+def _retrieve_latest_dataset_url():
+    try:
+        json_data = _get_json(API_URL)
+        json_resources = [
+            resource
+            for resource in json_data["resources"]
+            if resource["format"] == "json"
+        ]
+        if len(json_resources) == 0:
+            raise RuntimeError("Jeu de donnée JSON abenst.")
+        return json_resources[0]["latest"]
+    except (KeyError, IndexError, ValueError) as err:
+        raise RuntimeError(
+            f"Impossible de parser les données depuis {API_URL}:\n{err}"
+        )
+
+def _retrieve_json_data(dataset_url):
+    return _get_json(dataset_url)
+
+def _send_report(errors, imported, skipped):
+    mailer.mail_admins(
+        f"[{settings.SITE_NAME}] Rapport d'importation des centres de vaccination",
+        "mail/import_vaccination_notification.txt",
+        {
+            "errors": errors,
+            "imported": imported,
+            "skipped": skipped,
+            "SITE_NAME": settings.SITE_NAME,
+            "SITE_ROOT_URL": settings.SITE_ROOT_URL,
+        },
+    )
--- erp/management/commands/import_centres_vaccination.py
+++ erp/management/commands/import_centres_vaccination.py
@@ -1,11 +1,11 @@
 import requests
 import sys
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from erp.mapper.vaccination import RecordMapper
-from erp.models import Activite
-
+from core import mailer
+from erp.jobs import import_centres_vaccination
 
 def fatal(msg):
     print(msg)
@@ -13,10 +13,7 @@ def fatal(msg):
 
 
 class Command(BaseCommand):
-    "Imports vaccination centers from open data: https://www.data.gouv.fr/fr/datasets/lieux-de-vaccination-contre-la-covid-19/"
-
     help = "Importe les centres de vaccination COVID"
-    api_url = "https://www.data.gouv.fr/api/1/datasets/lieux-de-vaccination-contre-la-covid-19/"
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -29,77 +26,21 @@ class Command(BaseCommand):
             action="store_true",
             help="Afficher les raisons d'écartement d'import de certains centres",
         )
-
-    def get_json(self, url):
-        try:
-            return requests.get(url).json()
-        except requests.exceptions.RequestException as err:
-            raise RuntimeError(
-                f"Erreur de récupération des données JSON: {url}:\n  {err}"
-            )
-
-    def retrieve_latest_dataset_url(self):
-        try:
-            json_data = self.get_json(self.api_url)
-            json_resources = [
-                resource
-                for resource in json_data["resources"]
-                if resource["format"] == "json"
-            ]
-            if len(json_resources) == 0:
-                raise RuntimeError("Jeu de donnée JSON abenst.")
-            return json_resources[0]["latest"]
-        except (KeyError, IndexError, ValueError) as err:
-            raise RuntimeError(
-                f"Impossible de parser les données depuis {self.api_url}:\n{err}"
-            )
-
-    def retrieve_json_data(self, dataset_url):
-        return self.get_json(dataset_url)
+        parser.add_argument(
+            "--report",
+            action="store_true",
+            help="Envoi un mail en fin d'opération",
+        )
 
     def handle(self, *args, **options):  # noqa
         self.stdout.write("Importation des centres de vaccination")
 
+        verbose = options["verbose"] or False
+        dataset_url = options["dataset-url"] or ''
+
         try:
-            if "dataset-url" in options:
-                json_data = self.retrieve_json_data(options["dataset-url"])
-            else:
-                latest_dataset_url = self.retrieve_latest_dataset_url()
-                json_data = self.retrieve_json_data(latest_dataset_url)
+            import_centres_vaccination.job(dataset_url=dataset_url, verbose=verbose)
         except RuntimeError as err:
-            print(err)
-            sys.exit(1)
+            fatal(err)
 
-        activite = Activite.objects.filter(slug="centre-de-vaccination").first()
-        if not activite:
-            fatal("L'activité Centre de vaccination n'existe pas.")
 
-        errors = []
-        imported = 0
-        skipped = 0
-
-        if "features" not in json_data:
-            fatal("Liste des centres manquante")
-        for record in json_data["features"]:
-            try:
-                erp = RecordMapper(record).process(activite)
-                if options["verbose"]:
-                    print(f"- {erp.nom}\n  {erp.code_postal} {erp.commune_ext.nom}")
-                else:
-                    sys.stdout.write(".")
-                imported += 1
-            except RuntimeError as err:
-                if not options["verbose"]:
-                    sys.stdout.write("S")
-                errors.append(err)
-                sys.stdout.flush()
-                skipped += 1
-
-        if options["verbose"] and len(errors) > 0:
-            print("Erreurs rencontrées :")
-            for error in errors:
-                print(f"- {error}")
-
-        print("Opération effectuée:")
-        print(f"- {imported} centres importés")
-        print(f"- {skipped} écartés")
