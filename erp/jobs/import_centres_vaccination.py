647820738962120c92b2b192299bbdc500ded856
Calev Eliacheff <c.eliacheff@gmail.com>
2021-03-04 13:20:06 +0100

    use logger, print on local script, fixed reporting

--- erp/jobs/import_centres_vaccination.py
+++ erp/jobs/import_centres_vaccination.py
@@ -1,39 +1,47 @@
 "Imports vaccination centers from open data: https://www.data.gouv.fr/fr/datasets/lieux-de-vaccination-contre-la-covid-19/"
 
-import requests
-
 from django.conf import settings
+import requests
+import logging
 
 from core import mailer
 from erp.mapper.vaccination import RecordMapper
 from erp.models import Activite
 
+logger = logging.getLogger(__name__)
 
 API_URL = (
     "https://www.data.gouv.fr/api/1/datasets/lieux-de-vaccination-contre-la-covid-19/"
 )
 
 
-def job(dataset_url="", verbose=False, report=False):
+def job(dataset_url="", verbose=False, is_scheduler=False):
+    try:
+        errors, imported, skipped = do_import(dataset_url, is_scheduler)
+
+        if is_scheduler:
+            _send_report(errors, imported, skipped)
+        else:
+            if verbose and len(errors) > 0:
+                print("Erreurs rencontrées :")
+                for error in errors:
+                    print(f"- {error}")
+
+            print("Opération effectuée:")
+            print(f"- {imported} centres importés")
+            print(f"- {skipped} écartés")
+    except RuntimeError as err:
+        logger.error(err)
+
+
+def do_import(dataset_url, is_scheduler):
     json_data = _get_valid_data(dataset_url)
 
     activite = Activite.objects.filter(slug="centre-de-vaccination").first()
     if not activite:
         raise RuntimeError("L'activité Centre de vaccination n'existe pas.")
 
-    errors, imported, skipped = _process_data(json_data, activite, verbose)
-
-    if verbose and len(errors) > 0:
-        print("Erreurs rencontrées :")
-        for error in errors:
-            print(f"- {error}")
-
-    print("Opération effectuée:")
-    print(f"- {imported} centres importés")
-    print(f"- {skipped} écartés")
-
-    if report:
-        _send_report(errors, imported, skipped)
+    return _process_data(json_data, activite, is_scheduler)
 
 
 def _get_valid_data(dataset_url):
@@ -74,29 +82,28 @@ def _retrieve_json_data(dataset_url):
     return _get_json(dataset_url)
 
 
-def _process_data(records, activite, verbose=False):
+def _process_data(records, activite, is_scheduler=False):
     errors = []
     imported = 0
     skipped = 0
 
     for record in records:
         try:
-            erp = RecordMapper(record).process(activite)
-            if verbose:
-                print(f"- {erp.nom}\n  {erp.code_postal} {erp.commune_ext.nom}")
-            else:
+            RecordMapper(record).process(activite)
+            if not is_scheduler:
                 print(".", end="", flush=True)
             imported += 1
         except RuntimeError as err:
-            if not verbose:
+            if not is_scheduler:
                 print("S", end="", flush=True)
-            errors.append(err)
+            errors.append(err.__str__())
             skipped += 1
 
     return errors, imported, skipped
 
 
 def _send_report(errors, imported, skipped):
+    print(errors)
     mailer.mail_admins(
         f"[{settings.SITE_NAME}] Rapport d'importation des centres de vaccination",
         "mail/import_vaccination_notification.txt",
