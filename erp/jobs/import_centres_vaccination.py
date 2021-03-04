eca562b76c064b321ecc3ea80ff1eb1dbb2a131a
Calev Eliacheff <c.eliacheff@gmail.com>
2021-03-04 17:12:50 +0100

    refactor as class and output strategies

--- erp/jobs/import_centres_vaccination.py
+++ erp/jobs/import_centres_vaccination.py
@@ -1,4 +1,4 @@
-"Imports vaccination centers from open data: https://www.data.gouv.fr/fr/datasets/lieux-de-vaccination-contre-la-covid-19/"
+# Imports vaccination centers from open data: https://www.data.gouv.fr/fr/datasets/lieux-de-vaccination-contre-la-covid-19/
 
 from django.conf import settings
 import requests
@@ -10,108 +10,113 @@ from erp.models import Activite
 
 logger = logging.getLogger(__name__)
 
-API_URL = (
-    "https://www.data.gouv.fr/api/1/datasets/lieux-de-vaccination-contre-la-covid-19/"
-)
+
+def outputPrintStrategy(text, end="\n", flush=False):
+    print(text, end=end, flush=flush)
 
 
-def job(dataset_url="", verbose=False, is_scheduler=False):
-    try:
-        errors, imported, skipped = do_import(dataset_url, is_scheduler)
-
-        if is_scheduler:
-            _send_report(errors, imported, skipped)
-        else:
-            if verbose and len(errors) > 0:
-                print("Erreurs rencontrées :")
-                for error in errors:
-                    print(f"- {error}")
-
-            print("Opération effectuée:")
-            print(f"- {imported} centres importés")
-            print(f"- {skipped} écartés")
-    except RuntimeError as err:
-        logger.error(err)
+def outputVoidStrategy(text, end="\n", flush=False):
+    pass
 
 
-def do_import(dataset_url, is_scheduler):
-    json_data = _get_valid_data(dataset_url)
+class ImportVaccinationsCenters:
+    API_URL = "https://www.data.gouv.fr/api/1/datasets/lieux-de-vaccination-contre-la-covid-19/"
+    output = None
 
-    activite = Activite.objects.filter(slug="centre-de-vaccination").first()
-    if not activite:
-        raise RuntimeError("L'activité Centre de vaccination n'existe pas.")
+    def __init__(self, is_scheduler=False) -> None:
+        self.is_scheduler = is_scheduler
+        self.output = outputVoidStrategy if is_scheduler else outputPrintStrategy
 
-    return _process_data(json_data, activite, is_scheduler)
-
-
-def _get_valid_data(dataset_url):
-    if not dataset_url:
-        dataset_url = _retrieve_latest_dataset_url()
-
-    json_data = _retrieve_json_data(dataset_url)
-
-    if "features" not in json_data:
-        raise RuntimeError("Liste des centres manquante")
-
-    return json_data["features"]
-
-
-def _get_json(url):
-    try:
-        return requests.get(url).json()
-    except requests.exceptions.RequestException as err:
-        raise RuntimeError(f"Erreur de récupération des données JSON: {url}:\n  {err}")
-
-
-def _retrieve_latest_dataset_url():
-    try:
-        json_data = _get_json(API_URL)
-        json_resources = [
-            resource
-            for resource in json_data["resources"]
-            if resource["format"] == "json"
-        ]
-        if len(json_resources) == 0:
-            raise RuntimeError("Jeu de donnée JSON abenst.")
-        return json_resources[0]["latest"]
-    except (KeyError, IndexError, ValueError) as err:
-        raise RuntimeError(f"Impossible de parser les données depuis {API_URL}:\n{err}")
-
-
-def _retrieve_json_data(dataset_url):
-    return _get_json(dataset_url)
-
-
-def _process_data(records, activite, is_scheduler):
-    errors = []
-    imported = 0
-    skipped = 0
-
-    for record in records:
+    def job(self, dataset_url=None, verbose=False):
         try:
-            RecordMapper(record).process(activite)
-            if not is_scheduler:
-                print(".", end="", flush=True)
-            imported += 1
+            errors, imported, skipped = self.do_import(dataset_url)
+
+            if self.is_scheduler:
+                self._send_report(errors, imported, skipped)
+
+            if verbose and len(errors) > 0:
+                self.output("Erreurs rencontrées :")
+                for error in errors:
+                    self.output(f"- {error}")
+
+            self.output("Opération effectuée:")
+            self.output(f"- {imported} centres importés")
+            self.output(f"- {skipped} écartés")
         except RuntimeError as err:
-            if not is_scheduler:
-                print("S", end="", flush=True)
-            errors.append(err.__str__())
-            skipped += 1
+            logger.error(err)
 
-    return errors, imported, skipped
+    def do_import(self, dataset_url):
+        json_data = self._get_valid_data(dataset_url)
 
+        activite = Activite.objects.filter(slug="centre-de-vaccination").first()
+        if not activite:
+            raise RuntimeError("L'activité Centre de vaccination n'existe pas.")
 
-def _send_report(errors, imported, skipped):
-    print(errors)
-    mailer.mail_admins(
-        f"[{settings.SITE_NAME}] Rapport d'importation des centres de vaccination",
-        "mail/import_vaccination_notification.txt",
-        {
-            "errors": errors,
-            "imported": imported,
-            "skipped": skipped,
-            "SITE_NAME": settings.SITE_NAME,
-            "SITE_ROOT_URL": settings.SITE_ROOT_URL,
-        },
-    )
+        return self._process_data(json_data, activite, self.is_scheduler)
+
+    def _get_valid_data(self, dataset_url):
+        if not dataset_url:
+            dataset_url = self._retrieve_latest_dataset_url()
+
+        json_data = self._get_json(dataset_url)
+
+        if "features" not in json_data:
+            raise RuntimeError("Liste des centres manquante")
+
+        return json_data["features"]
+
+    def _get_json(self, url):
+        try:
+            return requests.get(url).json()
+        except requests.exceptions.RequestException as err:
+            raise RuntimeError(
+                f"Erreur de récupération des données JSON: {url}:\n  {err}"
+            )
+
+    def _retrieve_latest_dataset_url(self):
+        try:
+            json_data = self._get_json(self.API_URL)
+            json_resources = [
+                resource
+                for resource in json_data["resources"]
+                if resource["format"] == "json"
+            ]
+            if len(json_resources) == 0:
+                raise RuntimeError("Jeu de données absent.")
+            return json_resources[0]["latest"]
+        except (KeyError, IndexError, ValueError) as err:
+            raise RuntimeError(
+                f"Impossible de parser les données depuis {self.API_URL}:\n{err}"
+            )
+
+    def _process_data(self, records, activite, is_scheduler):
+        errors = []
+        imported = 0
+        skipped = 0
+
+        for record in records:
+            try:
+                RecordMapper(record).process(activite)
+                if not is_scheduler:
+                    self.output(".", "", True)
+                imported += 1
+            except RuntimeError as err:
+                if not is_scheduler:
+                    self.output("S", end="", flush=True)
+                errors.append(err.__str__())
+                skipped += 1
+
+        return errors, imported, skipped
+
+    def _send_report(self, errors, imported, skipped):
+        mailer.mail_admins(
+            f"[{settings.SITE_NAME}] Rapport d'importation des centres de vaccination",
+            "mail/import_vaccination_notification.txt",
+            {
+                "errors": errors,
+                "imported": imported,
+                "skipped": skipped,
+                "SITE_NAME": settings.SITE_NAME,
+                "SITE_ROOT_URL": settings.SITE_ROOT_URL,
+            },
+        )
