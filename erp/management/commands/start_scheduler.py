f2b3f0bee52dd5ab023315dd1f6ebca3dbfc1f43
Nicolas Perriault <nicolas@perriault.net>
2021-03-29 11:07:27 +0200

    Purge user accounts after 30 days without activation. (#152)
    * Purge user accounts after 30 days without activation.
    * Avoid purging inactive accounts with erps attached.

--- erp/management/commands/start_scheduler.py
+++ erp/management/commands/start_scheduler.py
@@ -3,7 +3,7 @@ import time
 
 from django.core.management.base import BaseCommand
 
-from erp.jobs import check_closed_erps
+from erp.jobs import check_closed_erps, purge_accounts
 from subscription.jobs import notify_changed_erps
 from erp.jobs.import_centres_vaccination import ImportVaccinationsCenters
 
@@ -14,6 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         schedule.every().day.at("00:00").do(check_closed_erps.job, verbose=True)
+        schedule.every().day.at("04:00").do(purge_accounts.job, verbose=True)
         schedule.every(notify_changed_erps.HOURS_CHECK).hours.do(
             notify_changed_erps.job, verbose=True
         )
