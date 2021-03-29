edca0b500579c980c9d7a366dc9b7764402b28c7
Nicolas Perriault <nicolas@perriault.net>
2021-03-29 11:27:13 +0200

    Improve stats accuracy regarding erps filled with a11y data. (#150)
    * Improve stats accuracy regarding erps filled with a11y data.
    * Add vaccination centers stats.

--- tests/stats/tests.py
+++ tests/stats/tests.py
@@ -3,9 +3,12 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
+from erp.models import Activite
+
 
 @pytest.mark.django_db
 def test_home():
     c = Client()
+    Activite.objects.create(nom="Centre de vaccination")
     response = c.get(reverse("stats_home"))
     assert response.status_code == 200
