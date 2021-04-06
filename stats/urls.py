e0c7c90b0feda18212661972cf2dcd38996e067a
Nicolas Perriault <nicolas@perriault.net>
2021-04-06 17:47:48 +0200

    Require auth to access goal metrics page.

--- stats/urls.py
+++ stats/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from core.cache import cache_per_user
 from . import views
 
 STATS_CACHE_TTL = 60 * 5
+
 
 urlpatterns = [
     path(
@@ -13,7 +15,9 @@ urlpatterns = [
     ),
     path(
         "objectifs/",
-        cache_per_user(STATS_CACHE_TTL)(views.ObjectifsView.as_view()),
+        cache_per_user(STATS_CACHE_TTL)(
+            staff_member_required(views.ObjectifsView.as_view())
+        ),
         name="stats_objectifs",
     ),
 ]
