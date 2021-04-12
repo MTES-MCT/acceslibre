e683b51442df0ecd7cb55cf4288909bbda19e968
Calev Eliacheff <c.eliacheff@gmail.com>
2021-04-12 09:41:30 +0200

    create schema mappers, with typechecking

---
+++ tests/erp/export/test_export_integration.py
@@ -0,0 +1,2 @@
+# TI: test csv file writing
+# TI: check frictionless validation with http call
---
+++ tests/erp/export/test_export_unit.py
@@ -0,0 +1,43 @@
+import csv
+from dataclasses import fields
+from io import StringIO
+from typing import List
+
+import pytest
+
+from erp.export.export import export_data
+from erp.export.mappers import map_erp_to_official_schema
+from erp.export.schemas import OfficialSchema
+from erp.models import Erp
+from tests.erp.test_managers import create_test_erp
+
+
+@pytest.fixture
+def example_data(db):
+    return [
+        create_test_erp('test 1', transport_station_presence=True, commentaire="simple commentaire"),
+        create_test_erp('test 2', transport_station_presence=False, commentaire="simple commentaire 2")
+    ]
+
+
+def test_export_to_csv(example_data: List[Erp]):
+    # map
+    first_row = [x.name for x in fields(OfficialSchema)]
+    headers, mapped_data = map_erp_to_official_schema(example_data)
+
+    # write to csv
+    file = StringIO()
+    export_data(file, headers, mapped_data)
+
+    # assert correct csv values
+    file.seek(0)
+    reader = csv.DictReader(file, fieldnames=first_row)
+    # Skip headers
+    next(reader)
+    erp_0 = next(reader)
+    assert erp_0['transport_station_presence'] == str(mapped_data[0].transport_station_presence)
+    assert erp_0['commentaire'] == str(mapped_data[0].commentaire)
+    erp_1 = next(reader)
+    assert erp_1['transport_station_presence'] == str(mapped_data[1].transport_station_presence)
+    assert erp_1['commentaire'] == str(mapped_data[1].commentaire)
+
