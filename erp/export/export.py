2a066b8d084f7876ee96aaaf240b8ec02d10adf7
Calev Eliacheff <c.eliacheff@gmail.com>
2021-04-12 14:31:38 +0200

    export to real csv

--- erp/export/export.py
+++ erp/export/export.py
@@ -1,8 +1,9 @@
 import csv
 from dataclasses import asdict
+from typing import List
 
 
-def export_data(file, headers, mapped_data):
+def export_to_csv(file, headers, mapped_data: List):
     csv_writer = csv.DictWriter(file, fieldnames=headers)
     csv_writer.writeheader()
 
--- erp/export/mappers.py
+++ erp/export/mappers.py
@@ -1,8 +1,10 @@
 from dataclasses import fields
-from typing import List, Tuple
+from typing import List, Tuple, Literal
+
+from django.db import models
 
 from erp.export.schemas import OfficialSchema, OfficialSchemaV2
-from erp.models import Erp
+from erp.models import Erp, Accessibilite
 from erp import schema
 
 
@@ -106,7 +108,7 @@ def map_erp_to_official_schema_v2(erps: List[Erp]) -> Tuple[List[str], List[Offi
 
         return result
 
-    def map_rampe(data):
+    def map_rampe(data) -> Literal["Absence", "Fixe", "Amovible"]:
         if data == schema.RAMPE_FIXE:
             return "Fixe"
         elif data == schema.RAMPE_AMOVIBLE:
@@ -169,11 +171,16 @@ def map_erp_to_official_schema_v2(erps: List[Erp]) -> Tuple[List[str], List[Offi
 
     results = []
     for erp in erps:
+        try:
+            erp.accessibilite
+        except Accessibilite.DoesNotExist:
+            continue
+
         o = OfficialSchemaV2(
             id=str(erp.id),
             accueil_personnels=map_accueil_personnels(erp.accessibilite.accueil_personnels),
             accueil_aide_audition=erp.accessibilite.accueil_equipements_malentendants_presence,
-            accueil_equipements_malentendants = map_accueil_equipements_malentendants(erp.accessibilite.accueil_equipements_malentendants),
+            accueil_equipements_malentendants=map_accueil_equipements_malentendants(erp.accessibilite.accueil_equipements_malentendants),
             accueil_prestations=erp.accessibilite.accueil_prestations,
             sanitaires_erp=erp.accessibilite.sanitaires_presence,
             sanitaires_adaptes=erp.accessibilite.sanitaires_adaptes,
@@ -205,7 +212,7 @@ def map_erp_to_official_schema_v2(erps: List[Erp]) -> Tuple[List[str], List[Offi
             cheminement_1_reperage_elts_vitre=erp.accessibilite.entree_vitree_vitrophanie,
             cheminement_1_systeme_guidage=map_systeme_guidage([]),
             cheminement_1_largeur_mini=erp.accessibilite.entree_largeur_mini,
-            entree_type="Site",
+            entree_type="Batiment",
             entree_plain_pied=erp.accessibilite.entree_plain_pied,
             entree_rampe=map_rampe(erp.accessibilite.entree_marches_rampe),
             entree_rampe_sonnette=erp.accessibilite.entree_balise_sonore,
@@ -229,62 +236,8 @@ def map_erp_to_official_schema_v2(erps: List[Erp]) -> Tuple[List[str], List[Offi
             entree_1_reperage_elts_vitre=erp.accessibilite.entree_vitree_vitrophanie,
             entree_1_signaletique=erp.accessibilite.entree_reperage,
             entree_1_controle_acces=map_controle_acces(erp.accessibilite.entree_dispositif_appel_type),
-            entree_1_type_porte=None, # TODO
+            entree_1_type_porte=None,  # TODO
             entree_1_accueil_visible=erp.accessibilite.accueil_visibilite,
-            # transport_station_presence=erp.accessibilite.transport_station_presence,
-            # transport_information=erp.accessibilite.transport_information,
-            # stationnement_presence=erp.accessibilite.stationnement_presence,
-            # stationnement_pmr=erp.accessibilite.stationnement_pmr,
-            # stationnement_ext_presence=erp.accessibilite.stationnement_ext_presence,
-            # stationnement_ext_pmr=erp.accessibilite.stationnement_ext_pmr,
-            # cheminement_ext_presence=erp.accessibilite.cheminement_ext_presence,
-            # cheminement_ext_terrain_accidente=erp.accessibilite.cheminement_ext_terrain_accidente,
-            # cheminement_ext_plain_pied=erp.accessibilite.cheminement_ext_plain_pied,
-            # cheminement_ext_ascenseur=erp.accessibilite.cheminement_ext_ascenseur,
-            # cheminement_ext_nombre_marches=erp.accessibilite.cheminement_ext_nombre_marches,
-            # cheminement_ext_reperage_marches=erp.accessibilite.cheminement_ext_reperage_marches,
-            # cheminement_ext_main_courante=erp.accessibilite.cheminement_ext_main_courante,
-            # cheminement_ext_rampe=erp.accessibilite.cheminement_ext_rampe,
-            # cheminement_ext_pente=erp.accessibilite.cheminement_ext_pente,
-            # cheminement_ext_devers=erp.accessibilite.cheminement_ext_devers,
-            # cheminement_ext_bande_guidage=erp.accessibilite.cheminement_ext_bande_guidage,
-            # cheminement_ext_retrecissement=erp.accessibilite.cheminement_ext_retrecissement,
-            # entree_reperage=erp.accessibilite.entree_reperage,
-            # entree_vitree=erp.accessibilite.entree_vitree,
-            # entree_vitree_vitrophanie=erp.accessibilite.entree_vitree_vitrophanie,
-            # entree_plain_pied=erp.accessibilite.entree_plain_pied,
-            # entree_ascenseur=erp.accessibilite.entree_ascenseur,
-            # entree_marches=erp.accessibilite.entree_marches,
-            # entree_marches_reperage=erp.accessibilite.entree_marches_reperage,
-            # entree_marches_main_courante=erp.accessibilite.entree_marches_main_courante,
-            # entree_marches_rampe=erp.accessibilite.entree_marches_rampe,
-            # entree_dispositif_appel=erp.accessibilite.entree_dispositif_appel,
-            # entree_dispositif_appel_type=erp.accessibilite.entree_dispositif_appel_type,
-            # entree_balise_sonore=erp.accessibilite.entree_balise_sonore,
-            # entree_aide_humaine=erp.accessibilite.entree_aide_humaine,
-            # entree_largeur_mini=erp.accessibilite.entree_largeur_mini,
-            # entree_pmr=erp.accessibilite.entree_pmr,
-            # entree_pmr_informations=erp.accessibilite.entree_pmr_informations,
-            # accueil_visibilite=erp.accessibilite.accueil_visibilite,
-            # accueil_personnels=erp.accessibilite.accueil_personnels,
-            # accueil_equipements_malentendants_presence=erp.accessibilite.accueil_equipements_malentendants_presence,
-            # accueil_equipements_malentendants=erp.accessibilite.accueil_equipements_malentendants,
-            # accueil_cheminement_plain_pied=erp.accessibilite.accueil_cheminement_plain_pied,
-            # accueil_cheminement_ascenseur=erp.accessibilite.accueil_cheminement_ascenseur,
-            # accueil_cheminement_nombre_marches=erp.accessibilite.accueil_cheminement_nombre_marches,
-            # accueil_cheminement_reperage_marches=erp.accessibilite.accueil_cheminement_reperage_marches,
-            # accueil_cheminement_main_courante=erp.accessibilite.accueil_cheminement_main_courante,
-            # accueil_cheminement_rampe=erp.accessibilite.accueil_cheminement_rampe,
-            # accueil_retrecissement=erp.accessibilite.accueil_retrecissement,
-            # accueil_prestations=erp.accessibilite.accueil_prestations,
-            # sanitaires_presence=erp.accessibilite.sanitaires_presence,
-            # sanitaires_adaptes=erp.accessibilite.sanitaires_adaptes,
-            # labels=erp.accessibilite.labels,
-            # labels_familles_handicap=erp.accessibilite.labels_familles_handicap,
-            # labels_autre=erp.accessibilite.labels_autre,
-            # commentaire=erp.accessibilite.commentaire,
-            # registre_url=erp.accessibilite.registre_url,
-            # conformite=erp.accessibilite.conformite
         )
 
         results.append(o)
