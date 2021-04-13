6418d2d90eaeaf69e04812e1a36a7f81df6045d0
Calev Eliacheff <c.eliacheff@gmail.com>
2021-04-13 09:08:52 +0200

    revert to our schema

--- erp/jobs/export_to_datagouv.py
+++ erp/jobs/export_to_datagouv.py
@@ -1,10 +1,10 @@
 from erp.export.export import export_to_csv
-from erp.export.mappers import map_erp_to_official_schema_v2
-from erp.models import Erp, Accessibilite
+from erp.export.mappers import map_erp_to_official_schema
+from erp.models import Erp
 
 
 def job(*args, **kwargs):
     with open('export.csv', 'w', newline='') as csv_file:
         erps = Erp.objects.select_related('accessibilite').filter(published=True)
-        headers, mapped_data = map_erp_to_official_schema_v2(erps)
+        headers, mapped_data = map_erp_to_official_schema(erps)
         export_to_csv(csv_file, headers, mapped_data)
