8ca40704ff0f04e91a6044379b27265376335f31
Nicolas Perriault <nicolas@perriault.net>
2021-04-15 09:14:52 +0200

    Migrate fontastic icons to icomoon. (#163)

--- static/scss/styles.js
+++ static/scss/styles.js
@@ -1,8 +1,8 @@
-import '../icons/styles.css'
-import 'leaflet/dist/leaflet.css'
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
-import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css'
+import "../icons/style.css";
+import "leaflet/dist/leaflet.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
 
-import '../vendor/selectWoo-1.0.8/css/select2.min.css'
-import '../vendor/selectWoo-1.0.8/css/select2-bootstrap4.min.css'
-import './style.scss';
+import "../vendor/selectWoo-1.0.8/css/select2.min.css";
+import "../vendor/selectWoo-1.0.8/css/select2-bootstrap4.min.css";
+import "./style.scss";
