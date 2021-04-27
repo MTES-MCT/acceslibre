a34b0b833d412d03417d9a584aa5449bce395bc8
Calev Eliacheff <c.eliacheff@gmail.com>
2021-04-27 12:56:23 +0200

    wip tests frictionless

--- erp/export/create_schema.py
+++ erp/export/create_schema.py
@@ -1,4 +1,11 @@
-from frictionless import Schema
+from erp import schema
+from frictionless import Schema, Field
 
 table_schema = Schema("base-schema.json")
+table_schema.add_field(
+    Field(
+        name="",
+        type="",
+    )
+)
 table_schema.to_json("test.json")
