8fb1c7af27ce4cbc749f1dafbe8ebd84a94ff438
celiacheff <c.eliacheff@gmail.com>
2021-05-31 09:40:01 +0200

    Export OpenData to CSV / data.gouv (#160)
    * create schema mappers, with typechecking
    
    * export to real csv
    
    * revert to our schema
    
    * refactoring
    
    * start remainings tests
    
    * refactoring
    
    * create schema mappers, with typechecking
    
    * export to real csv
    
    * revert to our schema
    
    * refactoring
    
    * start remainings tests
    
    * refactoring
    
    * unused import
    
    * wip retours
    
    * feedback fixes
    
    * wip local schema generation
    
    * wip tests frictionless
    
    * feedback on base schema
    
    * wip create schema manually
    
    * mapping types and enums/arrays
    
    * refactoring
    
    * bug fix + refacto
    
    * remove fun call
    
    * remove unused imports
    
    * Refactor + command
    
    * booleans
    
    * wip integrations tests
    
    * wip integrations tests 2
    
    * wip integrations tests 3 - correct paths, new data
    
    * fix stupid typo, working integration test
    
    * start add a test to block non-generated schema on changes
    
    * stable version, need some refactoring
    
    * fix typo, tests are up
    
    * wip removing presence-like fields
    
    * flake8
    
    * update conditionals fields
    
    * fix wrong exported values
    
    * revert all keys
    
    * add comment
    
    * more enums
    
    * schema update
    
    * retours Geoffrey, ajout de l'identification erp
    
    * wip maj export
    
    * new fields
    
    * maj mapper
    
    * maj mapper 2
    
    * maj fields
    
    * nico's feedback
    
    * fix flake8
    
    * imports
    
    * comment
    
    * refacto tests
    
    * updating to new schema structure
    
    * update schema
    
    * big refactoring
    
    * fix test
    
    * refactoring suite
    
    * rename
    
    * use uuid in the export
    
    * remove unpublished erps
    
    * superfluous call
    
    * cleaning, better testing
    
    * cleaning
    
    * cleaning 2

---
+++ erp/export/export.py
@@ -0,0 +1,22 @@
+import csv
+import json
+from dataclasses import asdict
+from typing import List, Type
+
+from erp.export.utils import BaseExportMapper, map_erps_to_json_schema
+from erp.models import Erp
+
+
+def factory(data):
+    # Lists in CSV are rendered like "[""value""]", but standard csv module gives '["value"]'
+    return dict([(x[0], json.dumps(x[1])) if type(x[1]) == list else x for x in data])
+
+
+def export_schema_to_csv(file, erps: List[Erp], model: Type[BaseExportMapper]):
+    headers, mapped_data = map_erps_to_json_schema(erps, model)
+    csv_writer = csv.DictWriter(file, fieldnames=headers)
+    csv_writer.writeheader()
+
+    for erp in mapped_data:
+        data = asdict(erp, dict_factory=factory)
+        csv_writer.writerow(data)
