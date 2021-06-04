07c085487d5bbc4e4e611ed7a619c02c0fa07430
Nicolas Perriault <nicolas@perriault.net>
2021-06-04 12:04:10 +0200

    Fix broken tests for recently updated template tags.

--- tests/erp/test_templatetags.py
+++ tests/erp/test_templatetags.py
@@ -9,13 +9,13 @@ from erp.templatetags import a4a
     "value, expected",
     [
         ("foo", "foo"),
-        (Distance(m=0), "Au même endroit"),
-        (Distance(m=22), "À 22 m"),
-        (Distance(m=1222), "À 1222 m"),
-        (Distance(m=1802), "À 1.80 km"),
-        (Distance(km=42), "À 42 km"),
-        (Distance(km=42.8), "À 43 km"),
+        (Distance(m=0), ["Au même endroit"]),
+        (Distance(m=22), ["À 22", "mètres"]),
+        (Distance(m=1222), ["À 1222", "mètres"]),
+        (Distance(m=1802), ["À 1,80", "kilomètres"]),
+        (Distance(km=42), ["À 42", "kilomètres"]),
+        (Distance(km=42.8), ["À 43", "kilomètres"]),
     ],
 )
 def test_format_distance(value, expected):
-    assert a4a.format_distance(value) == expected
+    assert all([test in a4a.format_distance(value) for test in expected]), expected
