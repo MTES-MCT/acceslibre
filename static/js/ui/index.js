8af39fff403cff71805b3bb239e2a5469c276909
Nicolas Perriault <nicolas@perriault.net>
2021-06-13 15:53:41 +0200

    Refactor ui imports.

---
+++ static/js/ui/index.js
@@ -0,0 +1,15 @@
+import AsteriskField from "./AsteriskField";
+import ConditionalForm from "./ConditionalForm.js";
+import GeoLink from "./GeoLink.js";
+import GetGeolocBtn from "./GetGeolocBtn";
+import MapExpander from "./MapExpander";
+import SearchWhere from "./SearchWhere";
+
+export default {
+  AsteriskField,
+  ConditionalForm,
+  GeoLink,
+  GetGeolocBtn,
+  MapExpander,
+  SearchWhere,
+};
