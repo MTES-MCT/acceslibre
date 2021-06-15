0a787f8e4a64acbfec521e4fd3b0b5c6d90ccf52
Nicolas Perriault <nicolas@perriault.net>
2021-06-15 16:59:57 +0200

    Migrate notify_changed_erps job to command.

--- erp/management/commands/start_scheduler.py
+++ erp/management/commands/start_scheduler.py
@@ -4,8 +4,6 @@ import time
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
-from subscription.jobs import notify_changed_erps
-
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -14,10 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         schedule.every().day.at("04:00").do(call_command, "purge_inactive_accounts")
         schedule.every().hour.do(call_command, "purge_tokens")
-        # TODO: move notify_changed_erps job to a subscription app command
-        schedule.every(notify_changed_erps.HOURS_CHECK).hours.do(
-            notify_changed_erps.job, verbose=True
-        )
+        schedule.every(3).hours.do(call_command("notify_changed_erps", hours=3))
         schedule.every().hour.do(call_command, "import_dataset", "vaccination")
         schedule.every().day.do(call_command, "import_dataset", "gendarmerie")
         print("Scheduler started")
