a33cecc0ae52c9afc2f068da1f29b6c5a343b173
Calev Eliacheff <c.eliacheff@gmail.com>
2021-06-15 09:21:53 +0200

    move to command

--- erp/management/commands/export_to_datagouv.py
+++ erp/management/commands/export_to_datagouv.py
@@ -1,12 +1,50 @@
+import requests
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from erp.jobs import export_to_datagouv
+from erp.export.export import export_schema_to_csv, upload_to_datagouv
+from erp.export.mappers import EtalabMapper
+from erp.models import Erp
 
 
 class Command(BaseCommand):
+    help = "Exporte vers datagouv"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--verbose",
+            action="store_true",
+            help="Afficher les erreurs",
+        )
+
     def handle(self, *args, **options):
-        try:
-            export_to_datagouv.job()
-            print("Data exported successfully to 'acceslibre.csv'")
-        except KeyboardInterrupt:
-            print("Interrompu.")
+        verbose = options.get("verbose", False)
+        csv_path = "acceslibre.csv"
+        with open(csv_path, "w", newline="") as csv_file:
+            try:
+                erps = Erp.objects.published().having_a11y_data()
+                export_schema_to_csv(csv_file, erps, EtalabMapper)
+                if verbose:
+                    print("Local export successful: 'acceslibre.csv'")
+                if settings.DATAGOUV_API_KEY:
+                    upload_to_datagouv(csv_path)
+                    ping_mattermost()
+                    if verbose:
+                        print("Dataset updated")
+
+            except Exception as err:
+                if settings.DATAGOUV_API_KEY:
+                    ping_mattermost(str(err))
+                raise err
+
+
+def ping_mattermost(error=None):
+    if not settings.MATTERMOST_HOOK:
+        return
+    status = error if error else "Aucune erreur rencontrée"
+    requests.post(
+        settings.MATTERMOST_HOOK,
+        json={
+            "text": f"Export vers datagouv: {status}",
+        },
+    )
--- erp/management/commands/start_scheduler.py
+++ erp/management/commands/start_scheduler.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
-from erp.jobs import check_closed_erps, purge_accounts, export_to_datagouv
+from erp.jobs import check_closed_erps, purge_accounts
 from subscription.jobs import notify_changed_erps
 
 
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         schedule.every().day.at("04:00").do(purge_accounts.job, verbose=True)
         schedule.every().hour.do(call_command, "purge_tokens")
         if settings.DATAGOUV_API_KEY:
-            schedule.every().day.at("01:00").do(export_to_datagouv.job)
+            schedule.every().day.at("01:00").do(call_command, "export_to_datagouv")
         schedule.every(notify_changed_erps.HOURS_CHECK).hours.do(
             notify_changed_erps.job, verbose=True
         )
