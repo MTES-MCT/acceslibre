111477d4e3092605b6a324f464a96f885c66a538
Nicolas Perriault <nicolas@perriault.net>
2021-06-21 15:37:24 +0200

    Enable sentry environments.

--- core/settings_recette.py
+++ core/settings_recette.py
@@ -1,7 +1,9 @@
 # flake8: noqa
 import os
+import sentry_sdk
 
 from .settings import *
+
 
 DEBUG = False
 
@@ -11,18 +13,19 @@ SITE_HOST = "recette-access4all.osc-fr1.scalingo.io"
 SITE_ROOT_URL = f"https://{SITE_HOST}"
 ALLOWED_HOSTS = [SITE_HOST]
 
+if SENTRY_DSN is not None:
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN, integrations=[DjangoIntegration()], environment="production"
+    )
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
         "LOCATION": "/var/tmp/django_cache",
     }
 }
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-#         "LOCATION": "cache_acceslibre",
-#     }
-# }
 
 # FIXME: removed because of a nasty bug with dist static assets
 STATICFILES_STORAGE = "core.storage.AppStaticFilesStorage"
