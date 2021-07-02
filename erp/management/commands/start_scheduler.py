6c9ef5b5f563805a2a8f45caa5af3247051a3117
Nicolas Perriault <nicolas@perriault.net>
2021-07-02 16:43:32 +0200

    Fix scheduled time format in scheduler.

--- erp/management/commands/start_scheduler.py
+++ erp/management/commands/start_scheduler.py
@@ -23,7 +23,7 @@ class Command(BaseCommand):
         schedule.every(3).hours.do(call_command, "notify_changed_erps", hours=3)
         schedule.every().hour.do(call_command, "import_dataset", "vaccination")
         schedule.every().day.do(call_command, "import_dataset", "gendarmerie")
-        schedule.every().day.at("9:30").do(call_command, "notify_unpublished_erps")
+        schedule.every().day.at("09:30").do(call_command, "notify_unpublished_erps")
         # Keep revisions from last 30 days and at least 20 from older changes
         schedule.every().day.do(call_command, "deleterevisions", keep=20, days=30)
         print("Scheduler started")
