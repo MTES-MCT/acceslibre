c0dfaa578b4faca45dac9d90e5d0214c214998da
Nicolas Perriault <nicolas@perriault.net>
2021-07-06 12:25:18 +0200

    Regroup unpublished erp notifications email sending. (#227)

--- erp/management/commands/start_scheduler.py
+++ erp/management/commands/start_scheduler.py
@@ -3,6 +3,7 @@ import schedule
 import time
 import traceback
 
+from django.conf import settings
 from django.core.management import call_command, CommandError
 from django.core.management.base import BaseCommand
 
@@ -24,12 +25,14 @@ class Command(BaseCommand):
         schedule.every().day.at("04:00").do(call_command, "purge_inactive_accounts")
         schedule.every().hour.do(call_command, "purge_tokens")
         schedule.every().day.at("01:00").do(call_command, "export_to_datagouv")
-        schedule.every(3).hours.do(call_command, "notify_changed_erps", hours=3)
         schedule.every().hour.do(call_command, "import_dataset", "vaccination")
         schedule.every().day.do(call_command, "import_dataset", "gendarmerie")
-        schedule.every().day.at("09:30").do(call_command, "notify_unpublished_erps")
         # Keep revisions from last 30 days and at least 20 from older changes
         schedule.every().day.do(call_command, "deleterevisions", keep=20, days=30)
+        # Do NOT periodically notify people from staging, because that would confuse them A LOT
+        if not settings.STAGING:
+            schedule.every(3).hours.do(call_command, "notify_changed_erps", hours=3)
+            schedule.every().day.at("09:30").do(call_command, "notify_unpublished_erps")
 
     def start(self):
         print("Scheduler started")
