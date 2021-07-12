bacb18961221d0b8882edeb9e159cfa3522f32cc
z3ntUx <87012105+z3ntUx@users.noreply.github.com>
2021-07-12 16:19:30 +0200

    Fix metadata dict human readable value display.

--- core/lib/diff.py
+++ core/lib/diff.py
@@ -1,15 +1,7 @@
-from django.contrib.gis.geos import Point
-
-
 def dict_diff_keys(old, new):
     diff = []
     for key, oldval in old.items():
         newval = new.get(key)
-        # Convert points to tuples of float to avoid GEOS ParseException
-        if isinstance(oldval, Point):
-            oldval = oldval.coords
-        if isinstance(newval, Point):
-            newval = newval.coords
         if newval != oldval:
             diff.append({"field": key, "old": oldval, "new": newval})
     return diff
