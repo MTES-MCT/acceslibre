4c412e02ae4de3011bdbbe476ca0148e8dd383ff
christophe <christophe@zentux.fr>
2022-02-15 09:14:06 +0100

    Design index page.

--- static/scss/styles.js
+++ static/scss/styles.js
@@ -14,5 +14,9 @@ import "leaflet.locatecontrol/dist/L.Control.Locate.min";
 import "../vendor/selectWoo-1.0.8/css/select2.min.css";
 import "../vendor/selectWoo-1.0.8/css/select2-bootstrap4.min.css";
 
+// DSFR
+import "../vendor/dsfr/dist/css/dsfr.css";
+
 // Acceslibre own styles
+import "./style-dsfr.scss";
 import "./style.scss";
