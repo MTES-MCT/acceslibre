6e08ed90917bca1a97af8892f44937864274b80f
christophe <christophe@zentux.fr>
2022-05-12 12:40:43 +0200

    Fix DSN sentry.

--- core/settings_prod.py
+++ core/settings_prod.py
@@ -20,7 +20,11 @@ if SENTRY_DSN is not None:
     from sentry_sdk.integrations.django import DjangoIntegration
 
     sentry_sdk.init(
-        dsn=SENTRY_DSN, integrations=[DjangoIntegration()], environment="production"
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+        environment="production",
     )
 
 CACHES = {
