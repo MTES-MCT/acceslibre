d3b29e4de4db0347eb646b45ced4f8616682f95b
Marie-Laure Vernay <mlvernay@gmail.com>
2022-11-21 17:44:53 +0100

    Fix one more test.

--- tests/erp/forms/test_ViewAccessibiliteForm.py
+++ tests/erp/forms/test_ViewAccessibiliteForm.py
@@ -26,7 +26,7 @@ def get_by_name(entries: dict, name: str):
 def test_ViewAccessibiliteForm_empty():
     form = forms.ViewAccessibiliteForm()
     data = form.get_accessibilite_data()
-    assert list(data.keys()) == []
+    assert list(data.keys()) == ["Entrée"], "Entrée is mandatory"
 
 
 def test_ViewAccessibiliteForm_filled():
