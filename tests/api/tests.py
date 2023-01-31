6eaf0490856ecf1976fa1b8d0d2da57fa48ca5f2
mlvernay <3692643+mlvernay@users.noreply.github.com>
2023-01-31 10:59:24 +0100

    Enhance API tests. (#388)

--- tests/api/tests.py
+++ tests/api/tests.py
@@ -1,4 +1,5 @@
 import json
+from unittest.mock import ANY
 
 import pytest
 from django.urls import reverse
@@ -10,127 +11,306 @@ def api_client():
     return APIClient()
 
 
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    "url, status_code",
-    [
-        (reverse("openapi-schema"), 200),
-        (reverse("apidocs"), 200),
-        (reverse("api-root"), 200),
-        (reverse("accessibilite-list"), 200),
-        (reverse("accessibilite-help"), 200),
-        (reverse("activite-list"), 200),
-        (reverse("activite-list") + "?commune=Foo", 200),
-        (reverse("erp-list"), 200),
-    ],
-)
-def test_api_urls_ok(url, status_code, api_client):
-    response = api_client.get(url)
-
-    assert response.status_code == status_code
-
-
-def test_endpoint_erp_list(data, api_client):
-    response = api_client.get(reverse("erp-list"))
-
-    content = json.loads(response.content)
-    assert content["count"] == 1
-    assert content["page_size"] == 20
-    erp_json = content["results"][0]
-    assert erp_json["nom"] == "Aux bons croissants"
-    assert erp_json["activite"]["nom"] == "Boulangerie"
-    assert erp_json["activite"]["slug"] == "boulangerie"
-    assert erp_json["code_postal"] == "34830"
-    assert "user" not in erp_json
-    assert erp_json["accessibilite"]["accueil"]["accueil_visibilite"] is None
-    assert erp_json["accessibilite"]["transport"]["transport_station_presence"] is None
-
-    # same request with clean
-    response = api_client.get(reverse("erp-list") + "?clean=true")
-    content = json.loads(response.content)
-    assert content["count"] == 1
-    assert content["page_size"] == 20
-    erp_json = content["results"][0]
-    assert erp_json["nom"] == "Aux bons croissants"
-    assert "transport" not in erp_json["accessibilite"]
-    assert "accueil_visibilite" not in erp_json["accessibilite"]["accueil"]
-    assert erp_json["accessibilite"]["accueil"]["sanitaires_presence"] is True
-    assert erp_json["accessibilite"]["accueil"]["sanitaires_adaptes"] is False
-
-    # same request with readable
-    response = api_client.get(reverse("erp-list") + "?readable=true")
-    content = json.loads(response.content)
-    erp_json = content["results"][0]
-    assert erp_json["accessibilite"]["datas"]["accueil"]["accueil_visibilite"] is None
-    assert (
-        erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_presence"]
-        == "Des sanitaires sont mis à disposition dans l'établissement"
+@pytest.mark.usefixtures("api_client")
+@pytest.mark.usefixtures("data")
+class TestApi:
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        "url, status_code",
+        [
+            (reverse("openapi-schema"), 200),
+            (reverse("apidocs"), 200),
+            (reverse("api-root"), 200),
+            (reverse("accessibilite-list"), 200),
+            (reverse("accessibilite-help"), 200),
+            (reverse("activite-list"), 200),
+            (reverse("activite-list") + "?commune=Foo", 200),
+            (reverse("erp-list"), 200),
+        ],
     )
-    assert (
-        erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_adaptes"]
-        == "Aucun sanitaire adapté mis à disposition dans l'établissement"
-    )
-    assert erp_json["accessibilite"]["datas"]["transport"]["transport_station_presence"] is None
+    def test_api_urls_ok(self, api_client, url, status_code):
+        response = api_client.get(url)
 
-    # same request with readable & clean
-    response = api_client.get(reverse("erp-list") + "?readable=true&clean=true")
-    content = json.loads(response.content)
-    erp_json = content["results"][0]
-    assert "accueil_visibilite" not in erp_json["accessibilite"]["datas"]["accueil"]
-    assert (
-        erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_presence"]
-        == "Des sanitaires sont mis à disposition dans l'établissement"
-    )
-    assert (
-        erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_adaptes"]
-        == "Aucun sanitaire adapté mis à disposition dans l'établissement"
-    )
-    assert "transport" not in erp_json["accessibilite"]["datas"]
+        assert response.status_code == status_code
 
 
-def test_endpoint_erp_list_page_size(data, api_client):
-    response = api_client.get(reverse("erp-list") + "?page_size=25")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 1
-    assert content["page_size"] == 25
+@pytest.mark.usefixtures("api_client")
+@pytest.mark.usefixtures("data")
+class TestErpApi:
+    def test_list(self, api_client, data):
+        response = api_client.get(reverse("erp-list"))
+
+        content = json.loads(response.content)
+        assert content["count"] == 1
+        assert content["page_size"] == 20
+        erp_json = content["results"][0]
+        assert erp_json["nom"] == "Aux bons croissants"
+        assert erp_json["activite"]["nom"] == "Boulangerie"
+        assert erp_json["activite"]["slug"] == "boulangerie"
+        assert erp_json["code_postal"] == "34830"
+        assert "user" not in erp_json
+        assert erp_json["accessibilite"]["accueil"]["accueil_visibilite"] is None
+        assert erp_json["accessibilite"]["transport"]["transport_station_presence"] is None
+
+        # same request with clean
+        response = api_client.get(reverse("erp-list") + "?clean=true")
+        content = json.loads(response.content)
+        assert content["count"] == 1
+        assert content["page_size"] == 20
+        erp_json = content["results"][0]
+        assert erp_json["nom"] == "Aux bons croissants"
+        assert "transport" not in erp_json["accessibilite"]
+        assert "accueil_visibilite" not in erp_json["accessibilite"]["accueil"]
+        assert erp_json["accessibilite"]["accueil"]["sanitaires_presence"] is True
+        assert erp_json["accessibilite"]["accueil"]["sanitaires_adaptes"] is False
+
+        # same request with readable
+        response = api_client.get(reverse("erp-list") + "?readable=true")
+        content = json.loads(response.content)
+        erp_json = content["results"][0]
+        assert erp_json["accessibilite"]["datas"]["accueil"]["accueil_visibilite"] is None
+        assert (
+            erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_presence"]
+            == "Des sanitaires sont mis à disposition dans l'établissement"
+        )
+        assert (
+            erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_adaptes"]
+            == "Aucun sanitaire adapté mis à disposition dans l'établissement"
+        )
+        assert erp_json["accessibilite"]["datas"]["transport"]["transport_station_presence"] is None
+
+        # same request with readable & clean
+        response = api_client.get(reverse("erp-list") + "?readable=true&clean=true")
+        content = json.loads(response.content)
+        erp_json = content["results"][0]
+        assert "accueil_visibilite" not in erp_json["accessibilite"]["datas"]["accueil"]
+        assert (
+            erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_presence"]
+            == "Des sanitaires sont mis à disposition dans l'établissement"
+        )
+        assert (
+            erp_json["accessibilite"]["datas"]["accueil"]["sanitaires_adaptes"]
+            == "Aucun sanitaire adapté mis à disposition dans l'établissement"
+        )
+        assert "transport" not in erp_json["accessibilite"]["datas"]
+
+    def test_list_page_size(self, api_client, data):
+        response = api_client.get(reverse("erp-list") + "?page_size=25")
+        content = json.loads(response.content)
+        assert len(content["results"]) == 1
+        assert content["page_size"] == 25
+
+    def test_list_qs(self, api_client, data):
+        response = api_client.get(reverse("erp-list") + "?q=croissants")
+        content = json.loads(response.content)
+        assert len(content["results"]) == 1
+
+        response = api_client.get(reverse("erp-list") + "?q=nexiste_pas")
+        content = json.loads(response.content)
+        assert len(content["results"]) == 0
+
+    def test_detail(self, api_client, data):
+        response = api_client.get(reverse("erp-detail", kwargs={"slug": data.erp.slug}))
+        assert response.json() == {
+            "url": "http://testserver/api/erps/aux-bons-croissants/",
+            "web_url": "http://testserver/app/34-jacou/a/boulangerie/erp/aux-bons-croissants/",
+            "uuid": ANY,
+            "activite": {"nom": "Boulangerie", "slug": "boulangerie"},
+            "nom": "Aux bons croissants",
+            "slug": "aux-bons-croissants",
+            "adresse": "4 grand rue 34830 Jacou",
+            "commune": "Jacou",
+            "code_insee": ANY,
+            "code_postal": "34830",
+            "geom": {"type": "Point", "coordinates": [3.9047933, 43.6648217]},
+            "siret": "52128577500016",
+            "telephone": None,
+            "site_internet": None,
+            "contact_email": None,
+            "contact_url": None,
+            "user_type": "system",
+            "accessibilite": {
+                "url": f"http://testserver/api/accessibilite/{data.erp.accessibilite.id}/",
+                "erp": "http://testserver/api/erps/aux-bons-croissants/",
+                "transport": {
+                    "transport_station_presence": None,
+                    "transport_information": None,
+                    "stationnement_presence": None,
+                    "stationnement_pmr": None,
+                    "stationnement_ext_presence": None,
+                    "stationnement_ext_pmr": None,
+                },
+                "cheminement_ext": {
+                    "cheminement_ext_presence": None,
+                    "cheminement_ext_terrain_stable": None,
+                    "cheminement_ext_plain_pied": None,
+                    "cheminement_ext_ascenseur": None,
+                    "cheminement_ext_nombre_marches": None,
+                    "cheminement_ext_sens_marches": None,
+                    "cheminement_ext_reperage_marches": None,
+                    "cheminement_ext_main_courante": None,
+                    "cheminement_ext_rampe": None,
+                    "cheminement_ext_pente_presence": None,
+                    "cheminement_ext_pente_degre_difficulte": None,
+                    "cheminement_ext_pente_longueur": None,
+                    "cheminement_ext_devers": None,
+                    "cheminement_ext_bande_guidage": None,
+                    "cheminement_ext_retrecissement": None,
+                },
+                "entree": {
+                    "entree_reperage": None,
+                    "entree_porte_presence": None,
+                    "entree_porte_manoeuvre": None,
+                    "entree_porte_type": None,
+                    "entree_vitree": None,
+                    "entree_vitree_vitrophanie": None,
+                    "entree_plain_pied": None,
+                    "entree_ascenseur": None,
+                    "entree_marches": None,
+                    "entree_marches_sens": None,
+                    "entree_marches_reperage": None,
+                    "entree_marches_main_courante": None,
+                    "entree_marches_rampe": None,
+                    "entree_dispositif_appel": None,
+                    "entree_dispositif_appel_type": [],
+                    "entree_balise_sonore": None,
+                    "entree_aide_humaine": None,
+                    "entree_largeur_mini": None,
+                    "entree_pmr": None,
+                    "entree_pmr_informations": None,
+                },
+                "accueil": {
+                    "accueil_visibilite": None,
+                    "accueil_cheminement_plain_pied": None,
+                    "accueil_cheminement_ascenseur": None,
+                    "accueil_cheminement_nombre_marches": None,
+                    "accueil_cheminement_sens_marches": None,
+                    "accueil_cheminement_reperage_marches": None,
+                    "accueil_cheminement_main_courante": None,
+                    "accueil_cheminement_rampe": None,
+                    "accueil_retrecissement": None,
+                    "accueil_personnels": None,
+                    "accueil_audiodescription_presence": None,
+                    "accueil_audiodescription": [],
+                    "accueil_equipements_malentendants_presence": None,
+                    "accueil_equipements_malentendants": [],
+                    "sanitaires_presence": True,
+                    "sanitaires_adaptes": False,
+                },
+                "registre": {"registre_url": None},
+                "conformite": {"conformite": None},
+                "commentaire": {
+                    "labels": [],
+                    "labels_familles_handicap": [],
+                    "labels_autre": None,
+                    "commentaire": None,
+                },
+            },
+            "distance": None,
+            "source_id": None,
+            "asp_id": None,
+        }
 
 
-def test_endpoint_erp_list_qs(data, api_client):
-    response = api_client.get(reverse("erp-list") + "?q=croissants")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 1
-
-    response = api_client.get(reverse("erp-list") + "?q=nexiste_pas")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 0
-
-
-def test_endpoint_erp_list_around(data, api_client):
-    response = api_client.get(reverse("erp-list") + "?around=45.4,4.4")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 0
-
-    response = api_client.get(reverse("erp-list") + "?around=43.66,3.91")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 1
+@pytest.mark.usefixtures("api_client")
+@pytest.mark.usefixtures("data")
+class TestActiviteApi:
+    def test_get(self, api_client, data):
+        response = api_client.get(reverse("activite-list"))
+        assert response.json() == {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [{"nom": "Boulangerie", "slug": "boulangerie", "count": 1}],
+        }
 
 
-def test_endpoint_erp_list_asp_id(data, api_client):
-    data.erp.asp_id = "abc"
-    data.erp.save()
-
-    response = api_client.get(reverse("erp-list") + "?asp_id=unknown")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 0
-
-    response = api_client.get(reverse("erp-list") + "?asp_id=abc")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 1
-
-    response = api_client.get(reverse("erp-list") + "?asp_id_not_null=true")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 1
-
-    response = api_client.get(reverse("erp-list") + "?asp_id_not_null=false")
-    content = json.loads(response.content)
-    assert len(content["results"]) == 0
+@pytest.mark.usefixtures("api_client")
+@pytest.mark.usefixtures("data")
+class TestAccessibiliteApi:
+    def test_get(self, api_client, data):
+        response = api_client.get(reverse("accessibilite-list"))
+        assert response.json() == {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "url": f"http://testserver/api/accessibilite/{data.erp.accessibilite.id}/",
+                    "erp": "http://testserver/api/erps/aux-bons-croissants/",
+                    "transport": {
+                        "transport_station_presence": None,
+                        "transport_information": None,
+                        "stationnement_presence": None,
+                        "stationnement_pmr": None,
+                        "stationnement_ext_presence": None,
+                        "stationnement_ext_pmr": None,
+                    },
+                    "cheminement_ext": {
+                        "cheminement_ext_presence": None,
+                        "cheminement_ext_terrain_stable": None,
+                        "cheminement_ext_plain_pied": None,
+                        "cheminement_ext_ascenseur": None,
+                        "cheminement_ext_nombre_marches": None,
+                        "cheminement_ext_sens_marches": None,
+                        "cheminement_ext_reperage_marches": None,
+                        "cheminement_ext_main_courante": None,
+                        "cheminement_ext_rampe": None,
+                        "cheminement_ext_pente_presence": None,
+                        "cheminement_ext_pente_degre_difficulte": None,
+                        "cheminement_ext_pente_longueur": None,
+                        "cheminement_ext_devers": None,
+                        "cheminement_ext_bande_guidage": None,
+                        "cheminement_ext_retrecissement": None,
+                    },
+                    "entree": {
+                        "entree_reperage": None,
+                        "entree_porte_presence": None,
+                        "entree_porte_manoeuvre": None,
+                        "entree_porte_type": None,
+                        "entree_vitree": None,
+                        "entree_vitree_vitrophanie": None,
+                        "entree_plain_pied": None,
+                        "entree_ascenseur": None,
+                        "entree_marches": None,
+                        "entree_marches_sens": None,
+                        "entree_marches_reperage": None,
+                        "entree_marches_main_courante": None,
+                        "entree_marches_rampe": None,
+                        "entree_dispositif_appel": None,
+                        "entree_dispositif_appel_type": [],
+                        "entree_balise_sonore": None,
+                        "entree_aide_humaine": None,
+                        "entree_largeur_mini": None,
+                        "entree_pmr": None,
+                        "entree_pmr_informations": None,
+                    },
+                    "accueil": {
+                        "accueil_visibilite": None,
+                        "accueil_cheminement_plain_pied": None,
+                        "accueil_cheminement_ascenseur": None,
+                        "accueil_cheminement_nombre_marches": None,
+                        "accueil_cheminement_sens_marches": None,
+                        "accueil_cheminement_reperage_marches": None,
+                        "accueil_cheminement_main_courante": None,
+                        "accueil_cheminement_rampe": None,
+                        "accueil_retrecissement": None,
+                        "accueil_personnels": None,
+                        "accueil_audiodescription_presence": None,
+                        "accueil_audiodescription": [],
+                        "accueil_equipements_malentendants_presence": None,
+                        "accueil_equipements_malentendants": [],
+                        "sanitaires_presence": True,
+                        "sanitaires_adaptes": False,
+                    },
+                    "registre": {"registre_url": None},
+                    "conformite": {"conformite": None},
+                    "commentaire": {
+                        "labels": [],
+                        "labels_familles_handicap": [],
+                        "labels_autre": None,
+                        "commentaire": None,
+                    },
+                }
+            ],
+        }
