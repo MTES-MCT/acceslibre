a8c02534df8f95ff57fd0db385adbd973b91a843
Marie-Laure Vernay <mlvernay@gmail.com>
2023-02-01 17:19:07 +0100

    Dummy commit.

--- makegraphs.sh
+++ makegraphs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 mkdir -p graphs
 ./manage.py graph_models -g -I Accessibilite,Activite,Erp,EquipementMalentendant,Label -o graphs/All.png
 ./manage.py graph_models --hide-edge-labels -n -I Accessibilite -o graphs/Accessibilite.png
