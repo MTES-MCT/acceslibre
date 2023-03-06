8afd197f966a37dea45ab6d3877a3456321b474d
mlvernay <3692643+mlvernay@users.noreply.github.com>
2023-03-06 10:38:57 +0100

    Fix user admin export. (#426)

--- compte/resources.py
+++ compte/resources.py
@@ -2,11 +2,14 @@ from django.contrib.auth.models import User
 from import_export import resources
 from import_export.fields import Field
 
+from compte.models import UserStats
+
 
 class UserAdminResource(resources.ModelResource):
-    erp_count_published = Field()
-    erp_count_total = Field()
     vote_count = Field()
+    nb_erp_created = Field()
+    nb_erp_edited = Field()
+    nb_erp_attributed = Field()
 
     class Meta:
         model = User
@@ -19,13 +22,25 @@ class UserAdminResource(resources.ModelResource):
             "is_active",
             "is_staff",
         ]
-        export_order = fields + ["erp_count_published", "erp_count_total", "vote_count"]
-
-    def dehydrate_erp_count_published(self, user):
-        return user.erp_count_published
-
-    def dehydrate_count_total(self, user):
-        return user.erp_count_total
+        export_order = fields + ["vote_count", "nb_erp_created", "nb_erp_edited", "nb_erp_attributed"]
 
     def dehydrate_vote_count(self, user):
         return user.vote_count
+
+    def dehydrate_nb_erp_created(self, user):
+        try:
+            return user.stats.nb_erp_created
+        except UserStats.DoesNotExist:
+            return "N/A"
+
+    def dehydrate_nb_erp_edited(self, user):
+        try:
+            return user.stats.nb_erp_edited
+        except UserStats.DoesNotExist:
+            return "N/A"
+
+    def dehydrate_nb_erp_attributed(self, user):
+        try:
+            return user.stats.nb_erp_attributed
+        except UserStats.DoesNotExist:
+            return "N/A"
