79c151c59f07355e2fb89663c27845dc7e11ac05
Marie-Laure Vernay <mlvernay@gmail.com>
2023-03-28 11:52:32 +0200

    Light robots.txt

--- core/views.py
+++ core/views.py
@@ -8,15 +8,5 @@ def robots_txt(request):
     lines = [
         "User-agent: *",
         f"Sitemap: {settings.SITE_ROOT_URL}/sitemap.xml",
-        "Crawl-delay: 1",
-        "",
-        "User-agent: PetalBot",
-        "Disallow: /",
-        "",
-        "User-agent: Screaming Frog SEO Spider",
-        "Disallow: /",
-        "",
-        "User-agent: AhrefsBot",
-        "Disallow: /",
     ]
     return HttpResponse("\n".join(lines), content_type="text/plain")
