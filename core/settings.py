44f2ac531bf453c8570c73a724a0004defbcbf01
mlvernay <3692643+mlvernay@users.noreply.github.com>
2023-06-02 10:36:04 +0200

    Translate free texts. (#558)
    Basic server side translation for free texts.
    This s2s solution avoid a malicious usage of our key/translation service.
    This is a naive approach which can be enhanced if needed, especially with
    some cache mechanisms.

--- core/settings.py
+++ core/settings.py
@@ -246,7 +246,8 @@ DATETIME_FORMAT = "Y-m-d, H:i:s"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
+DEEPL_API_KEY = env("DEEPL_API_KEY", default=None)
+DEEPL_MAPPING = {"en": "EN-GB"}
 # Crispy forms
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
