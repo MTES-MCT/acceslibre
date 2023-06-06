7cc6135e96764a0496dde37cc013fa2443f1b245
Marie-Laure Vernay <mlvernay@gmail.com>
2023-06-06 16:18:38 +0200

    Add some tests

--- tests/compte/test_commands.py
+++ tests/compte/test_commands.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
+from rest_framework_api_key.models import APIKey
 
 from erp.models import Erp
 
@@ -45,3 +47,18 @@ def test_purge_accounts_keep_inactive_with_erps(spambot):
     call_command("purge_obsolete_objects_in_base", today="2020-06-01")
 
     assert User.objects.filter(username="spambot").count() == 1
+
+
+@pytest.mark.django_db
+def test_purge_api_tokens():
+    to_keep, _ = APIKey.objects.create_key(name="to_not_delete", expiry_date=datetime.now() - timedelta(hours=2))
+    to_keep2, _ = APIKey.objects.create_key(
+        name=settings.INTERNAL_API_KEY_NAME, expiry_date=datetime.now() + timedelta(hours=1)
+    )
+    to_delete, _ = APIKey.objects.create_key(
+        name=settings.INTERNAL_API_KEY_NAME, expiry_date=datetime.now() - timedelta(hours=2)
+    )
+    call_command("purge_obsolete_objects_in_base")
+
+    assert APIKey.objects.filter(pk__in=(to_keep.pk, to_keep2.pk)).count() == 2
+    assert APIKey.objects.filter(pk=to_delete.pk).first() is None
