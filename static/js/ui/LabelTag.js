6b4acb583314a592905406401ed0a2535a4cebaf
Marie-Laure Vernay <mlvernay@gmail.com>
2023-07-04 14:05:37 +0200

    Filters in GET form.

--- static/js/ui/LabelTag.js
+++ static/js/ui/LabelTag.js
@@ -1,6 +1,6 @@
 function LabelTag(root) {
   root.addEventListener('click', function (event) {
-    let checkbox = document.getElementById(root.getAttribute('for-input'))
+    let checkbox = document.getElementById(root.getAttribute('data-for-input'))
     checkbox.checked = !checkbox.checked
   })
 }
