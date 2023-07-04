4c18463275b7b0db7998200f6f0e0adc24074a26
Anto59290 <antonin59290@hotmail.com>
2023-07-04 14:31:19 +0200

    Adapt to new attribute

--- static/js/ui/LabelTag.js
+++ static/js/ui/LabelTag.js
@@ -1,6 +1,6 @@
 function LabelTag(root) {
   root.addEventListener('click', function (event) {
-    let checkbox = document.getElementById(root.getAttribute('for'))
+    let checkbox = document.getElementById(root.getAttribute('for-input'))
     checkbox.checked = !checkbox.checked
   })
 }
