0cd330fe0c0702e1fb19b363455a59068d364d77
Anto59290 <antonin59290@hotmail.com>
2023-07-10 13:31:02 +0200

    Optimize API Key verification
    This commit optmize the time spent to verify an API key.
    
    Using two classes in `DEFAULT_PERMISSION_CLASSES` was causing an
    increase in latency for all API views. This is due to the fact that both
    the `HasAPIKey` and `IsAllowedForAction` classes are checking for the
    existence of the key using `get_from_key` (this calls a `is_valid`
    method which is doing some criptographic verifications that are slow).
    
    As the `IsAllowedForAction` class is already checking the key, we don't
    really need the `HasAPIKey` class.
    
    This commit also add a custom span so that in the future we know what is
    happening in the traces (avoiding a big blank in sentry)

--- api/permissions.py
+++ api/permissions.py
@@ -1,3 +1,4 @@
+import sentry_sdk
 from django.conf import settings
 from rest_framework import permissions
 from rest_framework_api_key.models import APIKey
@@ -9,9 +10,9 @@ class IsAllowedForAction(permissions.BasePermission):
     def has_permission(self, request, view):
         key = request.META["HTTP_AUTHORIZATION"].split()[1]
         try:
-            api_key = APIKey.objects.get_from_key(key)
+            with sentry_sdk.start_span(description="Check signature of API KEY"):
+                api_key = APIKey.objects.get_from_key(key)
         except APIKey.DoesNotExist:
-            # This case should not happen as this permission is intented to be used in combination with HasAPIKey
             return False
 
         if api_key.name == settings.INTERNAL_API_KEY_NAME and view.action not in ("default", "list"):
