52e2793eda66c7f60baa19f9c639e4fe33db29f6
Marie-Laure Vernay <mlvernay@gmail.com>
2023-07-10 18:11:30 +0200

    Be able to reuse same db in tests.

--- conftest.py
+++ conftest.py
@@ -23,6 +23,7 @@ def django_db_setup(django_db_setup, django_db_blocker):
         cursor.execute("CREATE EXTENSION IF NOT EXISTS postgis")
         cursor.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
         cursor.execute("CREATE EXTENSION IF NOT EXISTS unaccent")
+        cursor.execute("DROP TEXT SEARCH CONFIGURATION IF EXISTS french_unaccent cascade")
         cursor.execute("CREATE TEXT SEARCH CONFIGURATION french_unaccent( COPY = french )")
         cursor.execute(
             "ALTER TEXT SEARCH CONFIGURATION french_unaccent ALTER MAPPING FOR hword, hword_part, word WITH unaccent, french_stem"
