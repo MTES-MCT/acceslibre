2bd779bc34c73189201056bd236c081676164c02
Anto59290 <antonin59290@hotmail.com>
2023-07-17 15:27:30 +0200

    Allow to have multiple widgets on the same page
    This commit will allow third-parties to host multiple acceslibre widgets
    on the same page. That was not possible as most of the code made the
    assumption only one widget was loaded on every page.
    The main challenge of this commit is to keep the retro-compatibility
    with widgets that are already everywhere on internet.
    
    - Rewrite a good chunk of widget.js
    - Scope all the Javascript to make sure there is side effect on the
    hosting website
    - Make sure the link opens the correct modal and that everything still
    works (calling matomo, closing the modal, etc).
    
    Known issues:
    - Clicking outsite of the modal won't close it (as it was already the
    case)
    - All the environnements are calling the same Matomo instance

--- static/js/widget.js
+++ static/js/widget.js
@@ -1,169 +1,193 @@
-var erp_pk = document.getElementById('widget-a11y-container').getAttribute('data-pk')
-var base_url = document.getElementById('widget-a11y-container').getAttribute('data-baseurl')
-var opts = {
-  method: 'GET',
-  //headers: {'X-OriginUrl': window.location}
-}
+;(function () {
+  const base_url = document.querySelector('[data-baseurl]').getAttribute('data-baseurl')
+  const allContainers = document.querySelectorAll('[data-pk]')
+  const keyCodes = {
+    tab: 9,
+    enter: 13,
+    escape: 27,
+  }
+  const focusableElementsArray = [
+    '[href]',
+    'button:not([disabled])',
+    'input:not([disabled])',
+    'select:not([disabled])',
+    'textarea:not([disabled])',
+    '[tabindex]:not([tabindex="-1"])',
+  ]
 
-fetch(base_url + '/uuid/' + erp_pk + '/widget/', opts)
-  .then(function (response) {
-    return response.text()
-  })
-  .then(function (body) {
+  var itemsProcessed = 0
+
+  function closeModal(dialog, trigger) {
+    dialog.setAttribute('aria-hidden', true)
+    dialog.parentNode.setAttribute('aria-hidden', false)
+
+    // restoring focus
+    trigger.focus()
+  }
+
+  function openModal(dialog) {
     var _paq = (window._paq = window._paq || [])
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView'])
     _paq.push(['enableLinkTracking'])
     _paq.push(['enableHeartBeatTimer'])
-    _paq.push(['trackEvent', 'widget', 'display', true])
+    _paq.push(['trackEvent', 'widget', 'open', true])
     ;(function () {
       var u = '//stats.data.gouv.fr/'
       _paq.push(['setTrackerUrl', u + 'matomo.php'])
       _paq.push(['setSiteId', '118'])
-      var d = document,
-        g = d.createElement('script'),
-        s = d.getElementsByTagName('script')[0]
-      g.type = 'text/javascript'
-      g.async = true
-      g.defer = true
-      g.src = u + 'matomo.js'
-      s.parentNode.insertBefore(g, s)
     })()
+    const focusableElements = dialog.querySelectorAll(focusableElementsArray)
+    const firstFocusableElement = focusableElements[0]
+    const lastFocusableElement = focusableElements[focusableElements.length - 1]
 
-    var container = document.getElementById('widget-a11y-container')
-    var newDiv = document.createElement('div')
-    newDiv.innerHTML = body
-    container.appendChild(newDiv)
+    dialog.setAttribute('aria-hidden', false)
+    dialog.parentNode.setAttribute('aria-hidden', true)
 
-    const triggers = document.querySelectorAll('[aria-haspopup="dialog"]')
-    const doc = document.querySelector('.js-document')
-    const focusableElementsArray = [
-      '[href]',
-      'button:not([disabled])',
-      'input:not([disabled])',
-      'select:not([disabled])',
-      'textarea:not([disabled])',
-      '[tabindex]:not([tabindex="-1"])',
-    ]
-    const keyCodes = {
-      tab: 9,
-      enter: 13,
-      escape: 27,
+    // return if no focusable element
+    if (!firstFocusableElement) {
+      return
     }
 
-    const open = function (dialog) {
-      var _paq = (window._paq = window._paq || [])
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push(['trackPageView'])
-      _paq.push(['enableLinkTracking'])
-      _paq.push(['enableHeartBeatTimer'])
-      _paq.push(['trackEvent', 'widget', 'open', true])
-      ;(function () {
-        var u = '//stats.data.gouv.fr/'
-        _paq.push(['setTrackerUrl', u + 'matomo.php'])
-        _paq.push(['setSiteId', '118'])
-      })()
-      const focusableElements = dialog.querySelectorAll(focusableElementsArray)
-      const firstFocusableElement = focusableElements[0]
-      const lastFocusableElement = focusableElements[focusableElements.length - 1]
+    window.setTimeout(() => {
+      firstFocusableElement.focus()
 
-      dialog.setAttribute('aria-hidden', false)
-      doc.setAttribute('aria-hidden', true)
+      // trapping focus inside the dialog
+      focusableElements.forEach((focusableElement) => {
+        if (focusableElement.addEventListener) {
+          focusableElement.addEventListener('keydown', (event) => {
+            const tab = event.which === keyCodes.tab
 
-      // return if no focusable element
-      if (!firstFocusableElement) {
-        return
-      }
+            if (!tab) {
+              return
+            }
 
-      window.setTimeout(() => {
-        firstFocusableElement.focus()
-
-        // trapping focus inside the dialog
-        focusableElements.forEach((focusableElement) => {
-          if (focusableElement.addEventListener) {
-            focusableElement.addEventListener('keydown', (event) => {
-              const tab = event.which === keyCodes.tab
-
-              if (!tab) {
-                return
-              }
-
-              if (event.shiftKey) {
-                if (event.target === firstFocusableElement) {
-                  // shift + tab
-                  event.preventDefault()
-
-                  lastFocusableElement.focus()
-                }
-              } else if (event.target === lastFocusableElement) {
-                // tab
+            if (event.shiftKey) {
+              if (event.target === firstFocusableElement) {
+                // shift + tab
                 event.preventDefault()
 
-                firstFocusableElement.focus()
+                lastFocusableElement.focus()
               }
-            })
-          }
-        })
-      }, 100)
-    }
+            } else if (event.target === lastFocusableElement) {
+              // tab
+              event.preventDefault()
 
-    const close = function (dialog, trigger) {
-      dialog.setAttribute('aria-hidden', true)
-      doc.setAttribute('aria-hidden', false)
+              firstFocusableElement.focus()
+            }
+          })
+        }
+      })
+    }, 100)
+  }
 
-      // restoring focus
-      trigger.focus()
-    }
+  function loadTriggers() {
+    const triggers = document.querySelectorAll('[aria-haspopup="dialog"]')
 
     triggers.forEach((trigger) => {
-      const dialog = document.getElementById(trigger.getAttribute('aria-controls'))
+      var dialog = null
+      if (triggers.length == 1) {
+        // For retro compatibility purposes
+        dialog = document.getElementById(trigger.getAttribute('aria-controls'))
+      } else {
+        const triggerPk = trigger.getAttribute('data-erp-pk')
+        dialog = document.querySelector(`[data-pk="${triggerPk}"]`).querySelector('[id=dialog]')
+      }
       const dismissTriggers = dialog.querySelectorAll('[data-dismiss]')
 
-      // open dialog
       trigger.addEventListener('click', (event) => {
         event.preventDefault()
-        open(dialog)
+        openModal(dialog)
       })
 
       trigger.addEventListener('keydown', (event) => {
         if (event.which === keyCodes.enter) {
           event.preventDefault()
-          open(dialog)
+          openModal(dialog)
         }
       })
 
-      // close dialog
       dialog.addEventListener('keydown', (event) => {
         if (event.which === keyCodes.escape) {
-          close(dialog, trigger)
+          closeModal(dialog, trigger)
         }
       })
 
       dismissTriggers.forEach((dismissTrigger) => {
-        const dismissDialog = document.getElementById(dismissTrigger.dataset.dismiss)
+        const closeTriggerPk = dismissTrigger.dataset.dismiss
+        const dismissDialog = document.querySelector(`[data-pk="${closeTriggerPk}"]`).querySelector('[id=dialog]')
 
         dismissTrigger.addEventListener('click', (event) => {
           event.preventDefault()
 
-          close(dismissDialog, trigger)
+          closeModal(dismissDialog, trigger)
         })
       })
+    })
+  }
 
-      window.addEventListener('click', (event) => {
-        if (event.target === dialog) {
-          close(dialog, trigger)
+  function _trackOnClick(element, trackingString) {
+    element.addEventListener('click', () => {
+      var _paq = (window._paq = window._paq || [])
+      _paq.push(['trackEvent', 'widget', trackingString, true])
+    })
+  }
+
+  function trackEventOnClose() {
+    var close_buttons = document.querySelectorAll('.btn_acceslibre_close')
+    close_buttons.forEach(function (close_button) {
+      _trackOnClick(close_button, 'close')
+    })
+  }
+
+  function trackEventOnRedirect() {
+    var redirect_buttons = document.querySelectorAll('.btn_acceslibre')
+    redirect_buttons.forEach(function (redirect_button) {
+      _trackOnClick(redirect_button, 'redirect')
+    })
+  }
+
+  function finishSetup() {
+    loadTriggers()
+    trackEventOnClose()
+    trackEventOnRedirect()
+  }
+
+  allContainers.forEach(function (container) {
+    var erp_pk = container.getAttribute('data-pk')
+    fetch(base_url + '/uuid/' + erp_pk + '/widget/')
+      .then(function (response) {
+        return response.text()
+      })
+      .then(function (body) {
+        var _paq = (window._paq = window._paq || [])
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView'])
+        _paq.push(['enableLinkTracking'])
+        _paq.push(['enableHeartBeatTimer'])
+        _paq.push(['trackEvent', 'widget', 'display', true])
+        ;(function () {
+          var u = '//stats.data.gouv.fr/'
+          _paq.push(['setTrackerUrl', u + 'matomo.php'])
+          _paq.push(['setSiteId', '118'])
+          var d = document,
+            g = d.createElement('script'),
+            s = d.getElementsByTagName('script')[0]
+          g.type = 'text/javascript'
+          g.async = true
+          g.defer = true
+          g.src = u + 'matomo.js'
+          s.parentNode.insertBefore(g, s)
+        })()
+
+        var container = document.querySelector(`[data-pk="${erp_pk}"]`)
+        var newDiv = document.createElement('div')
+        newDiv.innerHTML = body
+        container.appendChild(newDiv)
+        itemsProcessed++
+        if (itemsProcessed == allContainers.length) {
+          finishSetup()
         }
       })
-      var btn_close = document.getElementById('btn_acceslibre_close')
-      btn_close.addEventListener('click', () => {
-        var _paq = (window._paq = window._paq || [])
-        _paq.push(['trackEvent', 'widget', 'close', true])
-      })
-
-      var btn_acceslibre_redirect = document.getElementById('btn_acceslibre')
-      btn_acceslibre_redirect.addEventListener('click', () => {
-        var _paq = (window._paq = window._paq || [])
-        _paq.push(['trackEvent', 'widget', 'redirect', true])
-      })
-    })
   })
+})()
