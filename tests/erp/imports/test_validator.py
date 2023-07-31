6ef48ba024b07afe0cec19a21a8abfc2ed2486d5
Marie-Laure Vernay <mlvernay@gmail.com>
2023-07-31 11:42:04 +0200

    Remove files created during tests.

--- tests/erp/imports/test_validator.py
+++ tests/erp/imports/test_validator.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 import pytest
 from django.core.management import CommandError, call_command
@@ -9,90 +10,92 @@ from erp.models import Erp
 from tests.erp.imports.mapper.fixtures import neufchateau
 
 
-def test_without_params_command():
-    with pytest.raises(CommandError):
-        call_command(Command())
+class TestValidator:
+    error_filename = "test_error.csv"
 
+    def test_without_params_command(self):
+        with pytest.raises(CommandError):
+            call_command(Command())
 
-def test_skip_import_with_KO_file():
-    cm = Command()
-    call_command(cm, file="data/tests/generic_test_failed.csv", skip_import=True)
+    def test_skip_import_with_KO_file(self):
+        cm = Command()
+        call_command(cm, file="data/tests/generic_test_failed.csv", skip_import=True)
 
-    assert cm.skip_import is True
-    assert cm.results["in_error"]["count"] != 0
-    assert cm.results["imported"]["count"] == 0
+        assert cm.skip_import is True
+        assert cm.results["in_error"]["count"] != 0
+        assert cm.results["imported"]["count"] == 0
 
+    def test_generate_error_file_with_KO_file(self, mocker):
+        cm = Command()
+        mocker.patch.object(cm, "_get_error_filename", return_value=self.error_filename)
+        call_command(cm, file="data/tests/generic_test_failed.csv", generate_errors_file=True)
 
-def test_generate_error_file_with_KO_file():
-    cm = Command()
-    call_command(cm, file="data/tests/generic_test_failed.csv", generate_errors_file=True)
+        assert cm.generate_errors_file is True
+        assert cm.error_file is not None
+        with open(cm.error_file_path, "r") as error_file:
+            reader = csv.DictReader(error_file, delimiter=";")
+            assert len(list(reader)) == 5
 
-    assert cm.generate_errors_file is True
-    assert cm.error_file is not None
-    with open(cm.error_file_path, "r") as error_file:
-        reader = csv.DictReader(error_file, delimiter=";")
-        assert len(list(reader)) == 5
+    def test_skip_import_with_OK_file(self, activite, neufchateau):
+        cm = Command()
+        call_command(cm, file="data/tests/generic_test_ok.csv", skip_import=True)
 
+        assert cm.skip_import is True
+        assert cm.results["in_error"]["count"] == 0
+        assert cm.results["validated"]["count"] == 1
 
-def test_skip_import_with_OK_file(activite, neufchateau):
-    cm = Command()
-    call_command(cm, file="data/tests/generic_test_ok.csv", skip_import=True)
+    def test_with_OK_file(self, activite, neufchateau):
+        cm = Command()
+        call_command(
+            cm,
+            file="data/tests/generic_test_ok.csv",
+        )
 
-    assert cm.skip_import is True
-    assert cm.results["in_error"]["count"] == 0
-    assert cm.results["validated"]["count"] == 1
+        assert cm.skip_import is False
+        assert cm.results["in_error"]["count"] == 0
+        assert cm.results["validated"]["count"] == 1
+        assert cm.results["imported"]["count"] == 1
+        erp = Erp.objects.last()
+        assert Version.objects.get_for_object(erp).count() == 1
 
+    def test_duplicate_with_OK_file(self, activite, neufchateau):
+        cm = Command()
+        call_command(
+            cm,
+            file="data/tests/generic_test_ok.csv",
+        )
 
-def test_with_OK_file(activite, neufchateau):
-    cm = Command()
-    call_command(
-        cm,
-        file="data/tests/generic_test_ok.csv",
-    )
+        assert cm.results["in_error"]["count"] == 0
+        assert cm.results["imported"]["count"] == 1
+        assert cm.results["duplicated"]["count"] == 0
 
-    assert cm.skip_import is False
-    assert cm.results["in_error"]["count"] == 0
-    assert cm.results["validated"]["count"] == 1
-    assert cm.results["imported"]["count"] == 1
-    erp = Erp.objects.last()
-    assert Version.objects.get_for_object(erp).count() == 1
+        call_command(
+            cm,
+            file="data/tests/generic_test_ok.csv",
+        )
 
+        assert cm.results["in_error"]["count"] == 0
+        assert cm.results["duplicated"]["count"] == 1
+        assert cm.results["imported"]["count"] == 0
 
-def test_duplicate_with_OK_file(activite, neufchateau):
-    cm = Command()
-    call_command(
-        cm,
-        file="data/tests/generic_test_ok.csv",
-    )
+    def test_duplicate_with_OK_file_force(self, activite, neufchateau):
+        cm = Command()
+        call_command(
+            cm,
+            file="data/tests/generic_test_ok.csv",
+        )
 
-    assert cm.results["in_error"]["count"] == 0
-    assert cm.results["imported"]["count"] == 1
-    assert cm.results["duplicated"]["count"] == 0
+        assert cm.results["in_error"]["count"] == 0
+        assert cm.results["imported"]["count"] == 1
+        assert cm.results["duplicated"]["count"] == 0
 
-    call_command(
-        cm,
-        file="data/tests/generic_test_ok.csv",
-    )
+        call_command(cm, file="data/tests/generic_test_ok.csv", force_update=True)
 
-    assert cm.results["in_error"]["count"] == 0
-    assert cm.results["duplicated"]["count"] == 1
-    assert cm.results["imported"]["count"] == 0
+        assert cm.results["in_error"]["count"] == 0
+        assert cm.results["duplicated"]["count"] == 1
+        assert cm.results["imported"]["count"] == 1
+        assert cm.results["validated"]["count"] == 1
 
-
-def test_duplicate_with_OK_file_force(activite, neufchateau):
-    cm = Command()
-    call_command(
-        cm,
-        file="data/tests/generic_test_ok.csv",
-    )
-
-    assert cm.results["in_error"]["count"] == 0
-    assert cm.results["imported"]["count"] == 1
-    assert cm.results["duplicated"]["count"] == 0
-
-    call_command(cm, file="data/tests/generic_test_ok.csv", force_update=True)
-
-    assert cm.results["in_error"]["count"] == 0
-    assert cm.results["duplicated"]["count"] == 1
-    assert cm.results["imported"]["count"] == 1
-    assert cm.results["validated"]["count"] == 1
+    def teardown_method(self, method):
+        if os.path.exists(self.error_filename):
+            os.remove(self.error_filename)
