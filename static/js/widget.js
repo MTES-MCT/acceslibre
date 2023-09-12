42082ce39619bb2ab0dd120a9cfd2b8bdb6f2178
Anto59290 <antonin59290@hotmail.com>
2023-09-12 15:32:02 +0200

    WIP

--- static/js/widget.js
+++ static/js/widget.js
@@ -1,202 +1,248 @@
-;(function () {
+const baseURLElement = document.querySelector('[data-baseurl]')
+if (!!baseURLElement) {
   const baseURL = document.querySelector('[data-baseurl]').getAttribute('data-baseurl')
-  const allContainers = document.querySelectorAll('[data-pk]')
-  const keyCodes = {
-    tab: 9,
-    enter: 13,
-    escape: 27,
-  }
-  const focusableElementsArray = [
-    '[href]',
-    'button:not([disabled])',
-    'input:not([disabled])',
-    'select:not([disabled])',
-    'textarea:not([disabled])',
-    '[tabindex]:not([tabindex="-1"])',
-  ]
+} else {
+  const baseURL = ''
+}
 
-  var itemsProcessed = 0
+const allContainers = document.querySelectorAll('[data-pk]')
+const keyCodes = {
+  tab: 9,
+  enter: 13,
+  escape: 27,
+}
+const focusableElementsArray = [
+  '[href]',
+  'button:not([disabled])',
+  'input:not([disabled])',
+  'select:not([disabled])',
+  'textarea:not([disabled])',
+  '[tabindex]:not([tabindex="-1"])',
+]
 
-  function closeModal(dialog, trigger) {
-    dialog.setAttribute('aria-hidden', true)
-    dialog.parentNode.setAttribute('aria-hidden', false)
+var itemsProcessed = 0
 
-    // restoring focus
+function closeModal(dialog, trigger) {
+  dialog.setAttribute('aria-hidden', true)
+  dialog.parentNode.setAttribute('aria-hidden', false)
+
+  // restoring focus
+  if (!!trigger) {
     trigger.focus()
   }
+}
 
-  function getTracker() {
-    if (window.AccesLibreMatomoTracker) {
-      return window.AccesLibreMatomoTracker
-    } else {
-      // In case Matomo is not available (eg: Adblock) return a mock to avoid errors
-      // in the rest of the code
-      return {
-        trackEvent: function () {},
-      }
+function getTracker() {
+  if (window.AccesLibreMatomoTracker) {
+    return window.AccesLibreMatomoTracker
+  } else {
+    // In case Matomo is not available (eg: Adblock) return a mock to avoid errors
+    // in the rest of the code
+    return {
+      trackEvent: function () {},
     }
   }
+}
 
-  function openModal(dialog) {
-    getTracker().trackEvent('widget', 'open', true)
-    const focusableElements = dialog.querySelectorAll(focusableElementsArray)
-    const firstFocusableElement = focusableElements[0]
-    const lastFocusableElement = focusableElements[focusableElements.length - 1]
+function openModal(dialog) {
+  getTracker().trackEvent('widget', 'open', true)
+  const focusableElements = dialog.querySelectorAll(focusableElementsArray)
+  const firstFocusableElement = focusableElements[0]
+  const lastFocusableElement = focusableElements[focusableElements.length - 1]
 
-    dialog.setAttribute('aria-hidden', false)
-    dialog.parentNode.setAttribute('aria-hidden', true)
+  dialog.setAttribute('aria-hidden', false)
+  dialog.parentNode.setAttribute('aria-hidden', true)
 
-    // return if no focusable element
-    if (!firstFocusableElement) {
-      return
-    }
+  // return if no focusable element
+  if (!firstFocusableElement) {
+    return
+  }
 
-    window.setTimeout(() => {
-      firstFocusableElement.focus()
+  window.setTimeout(() => {
+    firstFocusableElement.focus()
 
-      // trapping focus inside the dialog
-      focusableElements.forEach((focusableElement) => {
-        if (focusableElement.addEventListener) {
-          focusableElement.addEventListener('keydown', (event) => {
-            const tab = event.which === keyCodes.tab
+    // trapping focus inside the dialog
+    focusableElements.forEach((focusableElement) => {
+      if (focusableElement.addEventListener) {
+        focusableElement.addEventListener('keydown', (event) => {
+          const tab = event.which === keyCodes.tab
 
-            if (!tab) {
-              return
-            }
+          if (!tab) {
+            return
+          }
 
-            if (event.shiftKey) {
-              if (event.target === firstFocusableElement) {
-                // shift + tab
-                event.preventDefault()
-
-                lastFocusableElement.focus()
-              }
-            } else if (event.target === lastFocusableElement) {
-              // tab
+          if (event.shiftKey) {
+            if (event.target === firstFocusableElement) {
+              // shift + tab
               event.preventDefault()
 
-              firstFocusableElement.focus()
+              lastFocusableElement.focus()
             }
-          })
-        }
-      })
-    }, 100)
-  }
+          } else if (event.target === lastFocusableElement) {
+            // tab
+            event.preventDefault()
 
-  function loadTriggers() {
-    const triggers = document.querySelectorAll('[aria-haspopup="dialog"]')
-
-    triggers.forEach((trigger) => {
-      var dialog = null
-      if (triggers.length == 1) {
-        // For retro compatibility purposes
-        dialog = document.getElementById(trigger.getAttribute('aria-controls'))
-      } else {
-        const triggerPk = trigger.getAttribute('data-erp-pk')
-        dialog = document.querySelector(`[data-pk="${triggerPk}"]`).querySelector('[id=dialog]')
+            firstFocusableElement.focus()
+          }
+        })
       }
-      const dismissTriggers = dialog.querySelectorAll('[data-dismiss]')
+    })
+  }, 100)
+}
+// TODO remove me
+function _loadDismissTrigger(dismissTrigger, trigger = null) {
+  const closeTriggerPk = dismissTrigger.dataset.dismiss
+  const dismissDialog = document.querySelector(`[data-pk="${closeTriggerPk}"]`).querySelector('[id=dialog]')
 
-      trigger.addEventListener('click', (event) => {
+  dismissTrigger.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    closeModal(dismissDialog, trigger)
+  })
+}
+
+function loadTriggers() {
+  const triggers = document.querySelectorAll('[aria-haspopup="dialog"]')
+  console.log('in loadTriggers')
+  console.log(triggers)
+  triggers.forEach((trigger) => {
+    console.log('IN LOOP')
+    var dialog = null
+    if (triggers.length == 1) {
+      // For retro compatibility purposes
+      dialog = document.getElementById(trigger.getAttribute('aria-controls'))
+    } else {
+      const triggerPk = trigger.getAttribute('data-erp-pk')
+      dialog = document.querySelector(`[data-pk="${triggerPk}"]`).querySelector('[id=dialog]')
+    }
+    const dismissTriggers = dialog.querySelectorAll('[data-dismiss]')
+
+    trigger.addEventListener('click', (event) => {
+      event.preventDefault()
+      openModal(dialog)
+    })
+
+    trigger.addEventListener('keydown', (event) => {
+      if (event.which === keyCodes.enter) {
         event.preventDefault()
         openModal(dialog)
-      })
-
-      trigger.addEventListener('keydown', (event) => {
-        if (event.which === keyCodes.enter) {
-          event.preventDefault()
-          openModal(dialog)
-        }
-      })
-
-      dialog.addEventListener('keydown', (event) => {
-        if (event.which === keyCodes.escape) {
-          closeModal(dialog, trigger)
-        }
-      })
-
-      dismissTriggers.forEach((dismissTrigger) => {
-        const closeTriggerPk = dismissTrigger.dataset.dismiss
-        const dismissDialog = document.querySelector(`[data-pk="${closeTriggerPk}"]`).querySelector('[id=dialog]')
-
-        dismissTrigger.addEventListener('click', (event) => {
-          event.preventDefault()
-
-          closeModal(dismissDialog, trigger)
-        })
-      })
+      }
     })
-  }
 
-  function _trackOnClick(element, trackingString) {
-    element.addEventListener('click', () => {
-      getTracker().trackEvent('widget', trackingString, true)
+    dialog.addEventListener('keydown', (event) => {
+      if (event.which === keyCodes.escape) {
+        closeModal(dialog, trigger)
+      }
     })
-  }
 
-  function trackEventOnClose() {
-    var closeButtons = document.querySelectorAll('.btn_acceslibre_close')
-    closeButtons.forEach(function (closeButton) {
-      _trackOnClick(closeButton, 'close')
+    dismissTriggers.forEach((dismissTrigger) => {
+      _loadDismissTrigger(dismissTrigger, trigger)
     })
-  }
-
-  function trackEventOnRedirect() {
-    var redirectButtons = document.querySelectorAll('.btn_acceslibre')
-    redirectButtons.forEach(function (redirectButton) {
-      _trackOnClick(redirectButton, 'redirect')
-    })
-  }
-
-  function finishSetup() {
-    loadTriggers()
-    trackEventOnClose()
-    trackEventOnRedirect()
-  }
-
-  function setupAnalytics() {
-    var u = 'https://stats.data.gouv.fr/'
-    var matomoTracker = Matomo.getTracker(u + 'matomo.php', 118)
-    window.AccesLibreMatomoTracker = matomoTracker
-    matomoTracker.trackPageView()
-    matomoTracker.enableLinkTracking()
-    matomoTracker.enableHeartBeatTimer()
-  }
-  function setupAnalyticsScript() {
-    var u = 'https://stats.data.gouv.fr/'
-    ;(function () {
-      var d = document,
-        g = d.createElement('script'),
-        s = d.getElementsByTagName('script')[0]
-      g.type = 'text/javascript'
-      g.async = true
-      g.defer = true
-      g.src = u + 'matomo.js'
-      s.parentNode.insertBefore(g, s)
-
-      g.onreadystatechange = setupAnalytics
-      g.onload = setupAnalytics
-    })()
-  }
-
-  setupAnalyticsScript()
-
-  allContainers.forEach(function (container) {
-    var erpPK = container.getAttribute('data-pk')
-    fetch(baseURL + '/uuid/' + erpPK + '/widget/')
-      .then(function (response) {
-        return response.text()
-      })
-      .then(function (body) {
-        getTracker().trackEvent('widget', 'display', true)
-        var container = document.querySelector(`[data-pk="${erpPK}"]`)
-        var newDiv = document.createElement('div')
-        newDiv.innerHTML = body
-        container.appendChild(newDiv)
-        itemsProcessed++
-        if (itemsProcessed == allContainers.length) {
-          finishSetup()
-        }
-      })
   })
-})()
+}
+
+function _trackOnClick(element, trackingString) {
+  console.log('In _trackOnClick')
+  element.addEventListener('click', () => {
+    getTracker().trackEvent('widget', trackingString, true)
+  })
+}
+
+function trackEventOnClose() {
+  console.log('in trackEventOnClose')
+  var closeButtons = document.querySelectorAll('.btn_acceslibre_close')
+  console.log(closeButtons)
+  closeButtons.forEach(function (closeButton) {
+    _trackOnClick(closeButton, 'close')
+  })
+}
+
+function trackEventOnRedirect() {
+  var redirectButtons = document.querySelectorAll('.btn_acceslibre')
+  redirectButtons.forEach(function (redirectButton) {
+    _trackOnClick(redirectButton, 'redirect')
+  })
+}
+
+function finishSetup() {
+  loadTriggers()
+  trackEventOnClose()
+  trackEventOnRedirect()
+}
+
+function setupAnalytics() {
+  var u = 'https://stats.data.gouv.fr/'
+  var matomoTracker = Matomo.getTracker(u + 'matomo.php', 118)
+  window.AccesLibreMatomoTracker = matomoTracker
+  matomoTracker.trackPageView()
+  matomoTracker.enableLinkTracking()
+  matomoTracker.enableHeartBeatTimer()
+}
+function setupAnalyticsScript() {
+  var u = 'https://stats.data.gouv.fr/'
+  ;(function () {
+    var d = document,
+      g = d.createElement('script'),
+      s = d.getElementsByTagName('script')[0]
+    g.type = 'text/javascript'
+    g.async = true
+    g.defer = true
+    g.src = u + 'matomo.js'
+    s.parentNode.insertBefore(g, s)
+
+    g.onreadystatechange = setupAnalytics
+    g.onload = setupAnalytics
+  })()
+}
+
+function showAccessibilityModal(uuid) {
+  fetch('https://acceslibre.beta.gouv.fr"/uuid/' + uuid + '/widget/')
+    .then(function (response) {
+      return response.text()
+    })
+    .then(function (body) {
+      var newDiv = document.createElement('div')
+      newDiv.setAttribute('id', 'widget-a11y-container')
+      newDiv.innerHTML = body
+
+      bodyDom = document.getElementsByTagName('body')[0]
+      bodyDom.appendChild(newDiv)
+
+      dialogModal = newDiv.querySelector('#dialog')
+      dialogModal.setAttribute('aria-hidden', false)
+      dialogModal.parentNode.setAttribute('aria-hidden', true)
+
+      const dismissTrigger = dialogModal.querySelector('[data-dismiss]')
+      dismissTrigger.addEventListener('click', (event) => {
+        event.preventDefault()
+        closeModal(dialog, null)
+      })
+
+      dialogModal.addEventListener('keydown', (event) => {
+        if (event.which === keyCodes.escape) {
+          closeModal(dialog, null)
+        }
+      })
+    })
+}
+
+setupAnalyticsScript()
+
+allContainers.forEach(function (container) {
+  var erpPK = container.getAttribute('data-pk')
+  fetch(baseURL + '/uuid/' + erpPK + '/widget/')
+    .then(function (response) {
+      return response.text()
+    })
+    .then(function (body) {
+      getTracker().trackEvent('widget', 'display', true)
+      var container = document.querySelector(`[data-pk="${erpPK}"]`)
+      var newDiv = document.createElement('div')
+      newDiv.innerHTML = body
+      container.appendChild(newDiv)
+      itemsProcessed++
+      if (itemsProcessed == allContainers.length) {
+        finishSetup()
+      }
+    })
+})
