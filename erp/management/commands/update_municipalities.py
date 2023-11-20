679a23e84b2fa094e0f9d20705d7dc1005f23677
Antonin <antonin59290@hotmail.com>
2023-11-20 17:55:53 +0100

    Add script to create and update municipalities (#1047)
    Using the JSON file in order to make the process faster than using the
    API. The JSON file also contains the links between old municipalities
    and new ones (which is not the case for the API).
    
    - Write a command that will create new municipalities. Note that I had
    to get the geometry from the API to be able to create the municipality.
    - Update the existing municipalities (without touching geometry and
    contour, this will be done in a separate script).
    - Remap existing ERPs to new municipalities when possible
    - Remove previous command that was using an outdated source

---
+++ erp/management/commands/update_municipalities.py
@@ -0,0 +1,90 @@
+import json
+
+import requests
+from django.contrib.gis.geos import Point
+from django.core.management.base import BaseCommand
+
+from erp.dataclasses import Municipality
+from erp.models import Commune, Erp
+
+
+class Command(BaseCommand):
+    help = "Update all Commune objects based on official source"
+    updated_insee = []
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument(
+            "--write",
+            action="store_true",
+            help="Actually edit the database",
+        )
+
+    def _get_file(self):
+        remote_url = "https://unpkg.com/@etalab/decoupage-administratif@3.1.1/data/communes.json"
+        local_file = "communes.json"
+        data = requests.get(remote_url)
+        with open(local_file, "wb") as file:
+            file.write(data.content)
+
+    def _get_details(self, insee_code):
+        response = requests.get(f"https://geo.api.gouv.fr/communes/{insee_code}?fields=centre")
+        return response.json()
+
+    def _get_or_create_commune(self, data):
+        try:
+            commune = Commune.objects.get(code_insee=data.insee_code)
+        except Commune.DoesNotExist:
+            commune = Commune()
+            json = self._get_details(data.insee_code)
+            commune.geom = Point(json["centre"]["coordinates"][0], json["centre"]["coordinates"][1], srid=4326)
+            print(f"Will create commune {data.name}")
+        return commune
+
+    def _make_obsolete(self, old_insee, new_insee):
+        try:
+            commune = Commune.objects.get(code_insee=old_insee)
+        except Commune.DoesNotExist:
+            return
+
+        commune.obsolete = True
+
+        if new_insee:
+            erps = Erp.objects.filter(commune_ext=commune)
+            if erps:
+                new_commune = Commune.objects.get(code_insee=new_insee)
+                if self.write:
+                    erps.update(commune_ext=new_commune)
+        if self.write:
+            commune.save()
+
+    def handle(self, *args, **options):
+        self.write = options["write"]
+        self._get_file()
+        data = json.load(open("communes.json"))
+
+        for municipality in data:
+            validated_data = Municipality.from_json(municipality)
+
+            if validated_data.type_of not in ("commune-actuelle", "commune-deleguee"):
+                continue
+
+            if validated_data.type_of == "commune-actuelle":
+                if validated_data.postal_codes is None:
+                    # Allowed in the file but not in our project
+                    continue
+                commune = self._get_or_create_commune(validated_data)
+                commune.code_insee = validated_data.insee_code
+                commune.nom = validated_data.name
+                commune.departement = validated_data.departement
+                commune.code_postaux = validated_data.postal_codes
+                commune.population = validated_data.population
+                if self.write:
+                    commune.save()
+            elif validated_data.type_of == "commune-deleguee":
+                self._make_obsolete(validated_data.insee_code, validated_data.parent_municipality)
+
+            self.updated_insee.append(validated_data.insee_code)
+
+        unknown_communes = Commune.objects.exclude(code_insee__in=self.updated_insee)
+        print(f"Found {unknown_communes.count()} communes that were not found in the file")
+        print(list(unknown_communes))
---
+++ tests/erp/test_command_update_municipalities.py
@@ -0,0 +1,107 @@
+from copy import deepcopy
+from unittest.mock import MagicMock, patch
+
+import pytest
+from django.core.management import call_command
+
+from erp.models import Commune
+from tests.factories import CommuneFactory, ErpFactory
+
+LIST_JSON = [
+    {
+        "nom": "L'Abergement-Clémenciat",
+        "code": "01001",
+        "departement": "01",
+        "codesPostaux": ["01400"],
+        "population": 806,
+        "type": "commune-actuelle",
+    }
+]
+DETAILS_JSON = {
+    "centre": {"type": "Point", "coordinates": [2.1191, 48.8039]},
+}
+
+
+@pytest.fixture(scope="function", autouse=True)
+def mock_file(mocker):
+    mocker.patch("erp.management.commands.update_municipalities.Command._get_file", return_value=MagicMock())
+    mocker.patch("builtins.open", return_value=MagicMock())
+
+
+@pytest.mark.django_db
+@patch("erp.management.commands.update_municipalities.Command._get_details")
+@patch("erp.management.commands.update_municipalities.json.load")
+def test_command_will_create_unknown_commune(mocked_file, mocked_details):
+    mocked_file.return_value = LIST_JSON
+
+    mocked_details.return_value = DETAILS_JSON
+
+    assert Commune.objects.count() == 0
+    call_command("update_municipalities", write=True)
+
+    assert Commune.objects.count() == 1
+    commune = Commune.objects.get()
+
+    assert commune.nom == "L'Abergement-Clémenciat"
+    assert commune.code_insee == "01001"
+    assert commune.departement == "01"
+    assert commune.code_postaux == ["01400"]
+    assert commune.population == 806
+    assert commune.geom.coords == (2.1191, 48.8039)
+
+
+@pytest.mark.django_db
+@patch("erp.management.commands.update_municipalities.json.load")
+def test_command_will_update_commune(mocked_file):
+    CommuneFactory(
+        nom="Outdated commune",
+        code_insee="01001",
+        departement="02",
+        population=10,
+        code_postaux=["01401"],
+    )
+
+    mocked_file.return_value = LIST_JSON
+    call_command("update_municipalities", write=True)
+
+    commune = Commune.objects.get()
+
+    assert commune.nom == "L'Abergement-Clémenciat"
+    assert commune.code_insee == "01001"
+    assert commune.departement == "01"
+    assert commune.code_postaux == ["01400"]
+    assert commune.population == 806
+
+
+@pytest.mark.django_db
+@patch("erp.management.commands.update_municipalities.json.load")
+def test_make_obsolete_old_commune_without_erp(mocked_file):
+    commune = CommuneFactory(obsolete=False, code_insee="01001")
+    data = deepcopy(LIST_JSON)
+    data[0]["type"] = "commune-deleguee"
+    mocked_file.return_value = data
+
+    call_command("update_municipalities", write=True)
+
+    commune.refresh_from_db()
+    assert commune.obsolete is True
+
+
+@pytest.mark.django_db
+@patch("erp.management.commands.update_municipalities.json.load")
+def test_make_obsolete_old_commune_with_erp(mocked_file):
+    commune = CommuneFactory(obsolete=False, code_insee="01001")
+    new_commune = CommuneFactory(obsolete=False, code_insee="05005")
+    erp = ErpFactory(commune_ext=commune)
+
+    data = deepcopy(LIST_JSON)
+    data[0]["type"] = "commune-deleguee"
+    data[0]["chefLieu"] = "05005"
+    mocked_file.return_value = data
+
+    call_command("update_municipalities", write=True)
+
+    commune.refresh_from_db()
+    erp.refresh_from_db()
+    assert commune.obsolete is True
+    assert erp.commune_ext == new_commune
