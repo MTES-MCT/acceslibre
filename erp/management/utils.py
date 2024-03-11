2fdff89355a9cb5d6e7a65e78719b9602f9970be
Marie-Laure Vernay <mlvernay@gmail.com>
2024-03-11 12:05:47 +0100

    add print_warning

--- erp/management/utils.py
+++ erp/management/utils.py
@@ -9,3 +9,8 @@ def print_error(*args):
 def print_success(*args):
     print(Fore.GREEN, *args)
     print(Style.RESET_ALL)
+
+
+def print_warning(*args):
+    print(Fore.YELLOW, *args)
+    print(Style.RESET_ALL)
