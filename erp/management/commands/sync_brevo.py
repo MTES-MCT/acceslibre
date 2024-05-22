ae1c05627a0c587953a78663354f2dbf442223b0
mlvernay <3692643+mlvernay@users.noreply.github.com>
2024-05-22 09:42:25 +0200

    Fix import (#1440)

--- erp/management/commands/sync_brevo.py
+++ erp/management/commands/sync_brevo.py
@@ -1,7 +1,8 @@
-from datetime import timedelta, timezone
+from datetime import timedelta
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from core.mailer import BrevoMailer
 
