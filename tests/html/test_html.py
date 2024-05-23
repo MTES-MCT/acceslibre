7bd1753b7cfb68a1ef634614e9bc75e3c4853781
Marie-Laure Vernay <mlvernay@gmail.com>
2024-05-23 10:31:50 +0200

    Remove data fixture from tests

--- tests/html/test_html.py
+++ tests/html/test_html.py
@@ -5,6 +5,8 @@ import requests
 from django.test import Client
 from django.urls import reverse
 
+from tests.factories import ErpFactory, UserFactory
+
 
 @pytest.fixture
 def client():
@@ -12,9 +14,10 @@ def client():
 
 
 @pytest.fixture
-def auth_client(data):
+def auth_client():
+    admin = UserFactory(is_staff=True)
     client = Client()
-    client.force_login(data.admin)
+    client.force_login(admin)
     return client
 
 
@@ -52,45 +55,51 @@ def validate_url_get(client, url):
     validate_html(html)
 
 
-def test_home(data, client):
+@pytest.mark.django_db
+def test_home(client):
+    admin = UserFactory(is_staff=True)
     validate_url_get(client, reverse("home"))
-    client.force_login(data.admin)
+    client.force_login(admin)
     validate_url_get(client, reverse("home"))
 
 
-def test_home_auth(data, auth_client):
+@pytest.mark.django_db
+def test_home_auth(auth_client):
     validate_url_get(auth_client, reverse("home"))
 
 
-def test_search_empty(data, client):
+@pytest.mark.django_db
+def test_search_empty(client):
     validate_url_get(client, reverse("search"))
 
 
-def test_search_result(data, client, mocker):
+@pytest.mark.django_db
+def test_search_result(client, mocker):
     validate_url_get(client, reverse("search") + "?where=jacou")
 
 
-def fix_test_erp_details(data, client):
-    # TODO : Fix for button 'Tester le widget'
-    validate_url_get(client, data.erp.get_absolute_url())
+def fix_test_erp_details(client):
+    erp = ErpFactory()
+    validate_url_get(client, erp.get_absolute_url())
 
 
-def fix_test_erp_details_auth(data, auth_client):
-    # TODO : Fix for button 'Tester le widget'
-    validate_url_get(auth_client, data.erp.get_absolute_url())
+def fix_test_erp_details_auth(auth_client):
+    erp = ErpFactory()
+    validate_url_get(auth_client, erp.get_absolute_url())
 
 
-def test_editorial_accessibilite(data, client):
+def test_editorial_accessibilite(client):
     validate_url_get(client, reverse("accessibilite"))
 
 
-def test_editorial_cgu(data, client):
+def test_editorial_cgu(client):
     validate_url_get(client, reverse("cgu"))
 
 
-def test_editorial_partenaires(data, client):
+def test_editorial_partenaires(client):
     validate_url_get(client, reverse("partenaires"))
 
 
-def test_login(data, client):
+@pytest.mark.django_db
+def test_login(client):
     validate_url_get(client, reverse("login"))
