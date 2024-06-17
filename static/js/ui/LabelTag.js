ff743947ce87b7fd6a05785a8abd5bccf4af1666
Anto59290 <antonin59290@hotmail.com>
2024-06-17 10:46:57 +0200

    Fix filter not being applied on ERP page

--- static/js/ui/LabelTag.js
+++ static/js/ui/LabelTag.js
@@ -1,13 +1,10 @@
 function LabelTag(root) {
   root.addEventListener('click', function (event) {
-    const isEventFromUser = event.isTrusted
     let eventType = root.getAttribute('data-event-type')
     const input = event.target.parentElement.querySelector('input')
     input.checked = !input.checked
 
-    if (isEventFromUser === true) {
-      document.dispatchEvent(new CustomEvent(eventType, { detail: { source: event.target } }))
-    }
+    document.dispatchEvent(new CustomEvent(eventType, { detail: { source: event.target } }))
   })
 }
 
