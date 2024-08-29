ca89179b876605ae955e1be099b6f61c01860fb2
mlvernay <3692643+mlvernay@users.noreply.github.com>
2024-08-29 15:14:25 +0200

    Enhance homepage partners (links, translations, ...) (#1616)
    * Remove unused images
    
    * Add wheelchair image
    
    * Use partner constant
    
    - Use partner constant settings
    - Translate things
    
    * Rename accord into accor
    
    * Translations
    
    * Homepage wheelchair image.

--- erp/views.py
+++ erp/views.py
@@ -83,6 +83,7 @@ def home(request):
             "erps": Erp.objects.published(),
             "contributors": get_active_contributors_ids(),
             "latest": Erp.objects.select_related("activite", "commune_ext").published().order_by("-id")[:3],
+            "partners": schema.PARTENAIRES,
         },
     )
 
