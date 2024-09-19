5c780de1cfcb5553c164b8b415d6d6af4a7e63da
Marie-Laure Vernay <mlvernay@gmail.com>
2024-09-19 16:13:16 +0200

    Recompute leaderboard, async, when a user unsubscribe from a challenge.

---
+++ stats/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from stats.models import ChallengePlayer
+from stats.tasks import manage_challenge_player_unsubscription
+
+
+@receiver(post_delete, sender=ChallengePlayer)
+def challenge_player_deletion(sender, instance, **kwargs):
+    challenge_player_data = {
+        "username": instance.player.username,
+        "challenge_id": instance.challenge.pk,
+    }
+    manage_challenge_player_unsubscription.delay(challenge_player_data)
---
+++ tests/stats/test_models.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from stats.models import ChallengePlayer
+from tests.factories import ChallengeFactory, UserFactory
+
+
+class ChallengePlayerSignalTest(TestCase):
+    @patch("stats.tasks.manage_challenge_player_unsubscription.delay")
+    def test_challenge_player_deletion_signal(self, mock_tasks):
+        challenge = ChallengeFactory()
+        user = UserFactory()
+        player = ChallengePlayer.objects.create(challenge=challenge, player=user)
+        player.delete()
+
+        self.assertTrue(mock_tasks.called)
+        mock_tasks.assert_called_once_with({"username": user.username, "challenge_id": challenge.pk})
