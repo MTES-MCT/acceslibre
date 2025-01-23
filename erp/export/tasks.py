ffc7b8f55182fbe2388c41d3adc84c52288e39d3
Marie-Laure Vernay <mlvernay@gmail.com>
2025-01-23 15:35:31 +0100

    Upgrade boto to last version
    Related GH issue : https://github.com/boto/boto3/issues/4401

--- erp/export/tasks.py
+++ erp/export/tasks.py
@@ -3,6 +3,7 @@ import io
 from datetime import datetime, timezone
 
 import boto3
+from botocore.config import Config
 from celery import shared_task
 from django.conf import settings
 from django.http import QueryDict
@@ -32,7 +33,11 @@ def generate_csv_file(query_params, user_email, username):
     bucket_name = settings.S3_EXPORT_BUCKET_NAME
     file_name = f"export_{now}_{user_email_hash}.csv"
 
-    s3 = boto3.client("s3", endpoint_url=settings.S3_EXPORT_BUCKET_ENDPOINT_URL)
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=settings.S3_EXPORT_BUCKET_ENDPOINT_URL,
+        config=Config(request_checksum_calculation="when_required", response_checksum_validation="when_required"),
+    )
     s3.put_object(Bucket=bucket_name, Key=file_name, Body=csv_buffer.getvalue(), ContentType="text/csv")
 
     file_url = s3.generate_presigned_url(
