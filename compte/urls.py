5e9dd012fb5b9198cad27cd1f18d1eae32bc1b9c
Marie-Laure Vernay <mlvernay@gmail.com>
2025-03-27 16:41:35 +0100

    My profile

--- compte/urls.py
+++ compte/urls.py
@@ -4,9 +4,7 @@ from django.views.generic import TemplateView
 from compte import views
 
 urlpatterns = [
-    path("", views.mon_compte, name="mon_compte"),
-    path("identifiant/", views.mon_identifiant, name="mon_identifiant"),
-    path("email/", views.mon_email, name="mon_email"),
+    path("mon-profil/", views.my_profile, name="my_profile"),
     path("email/sent/", TemplateView.as_view(template_name="compte/mon_email_sent.html"), name="mon_email_sent"),
     path("email/change/<uuid:activation_token>/", views.change_email, name="change_email"),
     path("erps/", views.mes_erps, name="mes_erps"),
@@ -15,6 +13,5 @@ urlpatterns = [
     path("contributions/recues/", views.mes_contributions_recues, name="mes_contributions_recues"),
     path("challenges/", views.mes_challenges, name="mes_challenges"),
     path("suppression/", views.delete_account, name="delete_account"),
-    path("preferences/", views.mes_preferences, name="mes_preferences"),
     path("set-api-key/", views.set_api_key, name="set_api_key"),
 ]
