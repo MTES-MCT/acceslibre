9f23d7c97fb5cdb11bf6196e37fb89e80d2a2140
Marie-Laure Vernay <mlvernay@gmail.com>
2025-04-09 17:51:30 +0200

    Upgrade to Django 5.2

--- compte/management/commands/purge_tokens.py
+++ compte/management/commands/purge_tokens.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timezone
+from datetime import timezone
 
 from django.core.management.base import BaseCommand
 
@@ -12,5 +12,5 @@ class Command(BaseCommand):
     help = "Supprime les demandes de changements d'emails non utilisés de plus de 24h"
 
     def handle(self, *args, **options):
-        nb_deleted, _ = EmailToken.objects.filter(expire_at__lt=datetime.now(timezone.utc)).delete()
+        nb_deleted, _ = EmailToken.objects.filter(expire_at__lt=timezone.now()).delete()
         logger.info(f"[CRON] Purge tokens, {nb_deleted} token(s) deleted")
