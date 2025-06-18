95769f02557b46b570c9b8c2785698c7f7c76b3a
Marie-Laure Vernay <mlvernay@gmail.com>
2025-06-18 15:01:10 +0200

    Update XYZ and image id on rotation and position changed

--- static/js/panoramax.js
+++ static/js/panoramax.js
@@ -3,13 +3,18 @@ import '@panoramax/web-viewer'
 document.addEventListener('DOMContentLoaded', () => {
   const browser = document.querySelector('pnx-viewer')
   const xyzField = document.getElementById('xyz-coords')
+  const imageId = document.getElementById('image-id')
 
   if (browser && xyzField) {
-    browser.addEventListener('psv:view-rotated', (event) => {
+    const updateXYZandIMageID = (event) => {
       const { x, y, z } = event.detail
       const xyz = `${x}/${y}/${z}`
       xyzField.value = xyz
-    })
+      imageId.value = browser.getAttribute('picture')
+    }
+
+    browser.addEventListener('psv:view-rotated', updateXYZandIMageID)
+    browser.addEventListener('psv:position-changed', updateXYZandIMageID)
   }
 
   const viewer = document.querySelector('pnx-photo-viewer')
