1be5901fc64eca39b0e878038ee76bf07c48dde6
mlvernay <3692643+mlvernay@users.noreply.github.com>
2025-12-11 16:38:54 +0100

    cheminement_ext_pente_presence is child of cheminement_ext_plain_pied (#2377)

--- erp/schema.py
+++ erp/schema.py
@@ -958,6 +958,7 @@ FIELDS = {
             "cheminement_ext_main_courante",
             "cheminement_ext_rampe",
             "cheminement_ext_ascenseur",
+            "cheminement_ext_pente_presence",
         ],
         "value_to_display_children": ["False"],
     },
