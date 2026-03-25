c0d564988b5f39bf57e8731c8897a86ebfa35dda
mlvernay <3692643+mlvernay@users.noreply.github.com>
2026-03-25 17:06:22 +0100

    Change tile layer (#2558)
    Carto tiles are free. OSM is requiring a valid referer.

--- static/js/geo.js
+++ static/js/geo.js
@@ -197,7 +197,10 @@ function _displayCustomMenu(root, { latlng, target: map }) {
 function createMap(domTarget, options = {}) {
   const defaults = { layers: [getStreetTiles()], scrollWheelZoom: true, ...options }
   const map = L.map(domTarget, { ...defaults, options })
-
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+  }).addTo(map)
   L.control
     .layers({
       'Plan des rues': getStreetTiles(),
