f6b65d6ba729a339b6ebd153e5875ed48548fb51
Marie-Laure Vernay <mlvernay@gmail.com>
2026-07-08 10:41:02 +0200

    Edit checked_up_to_date_at date while editing, creating, importing erp

--- erp/models.py
+++ erp/models.py
@@ -689,7 +689,7 @@ class Erp(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=translate_lazy("Date de création"))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=translate_lazy("Dernière modification"))
     checked_up_to_date_at = models.DateTimeField(
-        null=True, blank=True, verbose_name=translate_lazy("Dernière vérification des informations")
+        auto_now_add=True, null=True, blank=True, verbose_name=translate_lazy("Dernière vérification des informations")
     )
     check_closed_at = models.DateTimeField(
         null=True, blank=True, verbose_name=translate_lazy("Dernière vérification de clôture")
