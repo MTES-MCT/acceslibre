0b789e3c78e3b627adb881d9105297dddfdabaac
Marie-Laure Vernay <mlvernay@gmail.com>
2026-09-16 11:09:17 +0200

    Define SECURE_HSTS_SECONDS on prod

--- core/settings_recette.py
+++ core/settings_recette.py
@@ -29,12 +29,6 @@ if SENTRY_DSN is not None:
 # FIXME: removed because of a nasty bug with dist static assets
 STATICFILES_STORAGE = "core.storage.AppStaticFilesStorage"
 
-# https://docs.djangoproject.com/fr/3.1/ref/middleware/#http-strict-transport-security
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 3600
-
 BREVO_TEMPLATE_IDS = {
     "draft_deleted": 27,
     "spam_activities_suggestion": 24,
